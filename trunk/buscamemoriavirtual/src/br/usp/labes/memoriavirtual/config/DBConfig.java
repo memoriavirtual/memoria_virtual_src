d97ad833cf523e575c337f220c803c8790a342a7
tbianchi <tbianchi@699894a6-35f8-415d-b816-a1f94c44af83>
2010-09-02 19:04:07 +0000

    git-svn-id: file:///home/brauner/Desktop/DEV_DUMP_19.05.2015/svn_storage/memoria_virtual_src@50 699894a6-35f8-415d-b816-a1f94c44af83

--- trunk/buscamemoriavirtual/src/br/usp/labes/memoriavirtual/config/DBConfig.java
+++
@@ -1,8 +0,0 @@
-package br.usp.labes.memoriavirtual.config;
-
-public interface DBConfig {
-	public String driver = "org.postgresql.Driver";
-	public String url = "jdbc:postgres://127.0.0.1:5432/memoria_virtual/";
-	public String username = "postgres";
-	public String password = "zion";
-}
