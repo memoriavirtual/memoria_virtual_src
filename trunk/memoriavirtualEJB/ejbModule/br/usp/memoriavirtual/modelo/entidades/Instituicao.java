7bd79afa31a945208d6c8dbdea15c73a41a4e89d
gmarques <gmarques@699894a6-35f8-415d-b816-a1f94c44af83>
2011-04-22 19:35:45 +0000

    Cria classe Instituição.
    git-svn-id: file:///home/brauner/Desktop/DEV_DUMP_19.05.2015/svn_storage/memoria_virtual_src@124 699894a6-35f8-415d-b816-a1f94c44af83

---
+++ trunk/memoriavirtualEJB/ejbModule/br/usp/memoriavirtual/modelo/entidades/Instituicao.java
@@ -0,0 +1,79 @@
+package br.usp.memoriavirtual.modelo.entidades;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Instituicao implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	@Id
+	private String numRegistro;
+	private String nome;
+	private String email;
+
+	/**
+	 * Construtor padrão
+	 */
+	public Instituicao() {
+		super();
+	}
+
+
+	public Instituicao(String numRegistro, String nome) {
+		this();
+		this.numRegistro = numRegistro;
+		this.nome = nome;
+	
+	}
+
+	/**
+	 * @return Numero de registro da instituição
+	 */
+	public String getNumRegistro() {
+		return numRegistro;
+	}
+
+	/**
+	 * @param numRegistro
+	 *            Define o numero de registro da instituição
+	 */
+	public void setnumRegistro(String numRegistro) {
+		this.numRegistro = numRegistro;
+	}
+
+	/**
+	 * @return O nome da instituição
+	 */
+	public String getNome() {
+		return nome;
+	}
+
+	/**
+	 * @param nome
+	 *            Define o nome da instituição
+	 */
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	/**
+	 * @return O email da instituição
+	 */
+	public String getEmail() {
+		return email;
+	}
+	
+	/**
+	 * @param email
+	 *            Define o email da instituição
+	 */
+	public void setEmail(String email) {
+		this.email = email;
+	}	
+}
