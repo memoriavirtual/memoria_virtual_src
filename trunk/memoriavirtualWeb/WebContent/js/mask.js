d0b4d14de19c978a6711d61b23c7db39a117b7eb
joesantos <joesantos@699894a6-35f8-415d-b816-a1f94c44af83>
2011-07-16 19:30:05 +0000

    Jquery atualizado, adicionado script para validação de formulário e plugin do Jquery necessário para tal
    git-svn-id: file:///home/brauner/Desktop/DEV_DUMP_19.05.2015/svn_storage/memoria_virtual_src@186 699894a6-35f8-415d-b816-a1f94c44af83

---
+++ trunk/memoriavirtualWeb/WebContent/js/mask.js
@@ -0,0 +1,22 @@
+/* Mascara para os formularios: 
+ *  $("# id_do_campo_a_mascarar").mask("9999/999/9");
+ *  
+ *  Numeros 9 onde só entram números, letras a onde só entram letras 
+ *  usa-se barras, parêntesis ou traços como separadores e ? para marcar preenchimento opcional
+ *  
+ *  Como opção um placeholder pode ser configurado
+ *  
+ *  jQuery(function($){
+   		$("#product").mask("99/99/9999",{placeholder:" "});
+	});
+ * 
+ *  Confira http://digitalbush.com/projects/masked-input-plugin/ para mais informações
+ * */
+
+jQuery(function($){
+
+   $("#estado").mask("aa", {completed:function(){this.val().toUpperCase()}});
+   $("#cep").mask("(999)99999-999");
+   $("#telefone").mask("(999)9999-9999");
+
+});
