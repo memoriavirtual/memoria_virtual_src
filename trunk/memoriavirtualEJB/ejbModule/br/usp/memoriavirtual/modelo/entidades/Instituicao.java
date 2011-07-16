3210b8fcf0d2c9da7e04be457f099e4e1d492302
joesantos <joesantos@699894a6-35f8-415d-b816-a1f94c44af83>
2011-07-16 21:45:50 +0000

    Alterada a entidade instituição de acordo com o CDU 31
    git-svn-id: file:///home/brauner/Desktop/DEV_DUMP_19.05.2015/svn_storage/memoria_virtual_src@188 699894a6-35f8-415d-b816-a1f94c44af83

--- trunk/memoriavirtualEJB/ejbModule/br/usp/memoriavirtual/modelo/entidades/Instituicao.java
+++ trunk/memoriavirtualEJB/ejbModule/br/usp/memoriavirtual/modelo/entidades/Instituicao.java
@@ -10,9 +10,7 @@ import javax.validation.constraints.NotNull;
 @Entity
 public class Instituicao implements Serializable{
 
-	/**
-	 * 
-	 */
+	
 	private static final long serialVersionUID = 1L;
 	@Id	
 	private String numeroRegistro;
@@ -21,6 +19,18 @@ public class Instituicao implements Serializable{
 	private String nome;
 	@Column(unique = true)
 	private String email;
+	@Column(unique = true)
+	private String localizacao;
+	@Column(unique = true)
+	private String endereco;
+	@Column(unique = true)
+	private String cidade;
+	@Column(unique = true)
+	private String estado;
+	@Column(unique = true)
+	private String cep;
+	@Column(unique = true)
+	private String telefone;
 
 	/**
 	 * Construtor padrão
@@ -35,7 +45,7 @@ public class Instituicao implements Serializable{
 		this.nome = nome;
 	
 	}
-
+	
 	/**
 	 * @return Numero de registro da instituição
 	 */
@@ -79,5 +89,95 @@ public class Instituicao implements Serializable{
 	 */
 	public void setEmail(String email) {
 		this.email = email;
+	}
+	
+	/**
+	 * @return A localizacao da instituição
+	 */
+	public String getLocalizacao() {
+		return localizacao;
+	}
+	
+	/**
+	 * @param localizacao
+	 *            Define a localizacao da instituição
+	 */
+	public void setLocalizacao(String localizacao) {
+		this.localizacao = localizacao;
+	}
+	
+	/**
+	 * @return O endereco da instituição
+	 */
+	public String getEndereco() {
+		return endereco;
+	}
+	
+	/**
+	 * @param endereco
+	 *            Define o endereco da instituição
+	 */
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+	
+	/**
+	 * @return A cidade da instituição
+	 */
+	public String getCidade() {
+		return cidade;
+	}
+	
+	/**
+	 * @param cidade
+	 *            Define a cidade da instituição
+	 */
+	public void setCidade(String cidade) {
+		this.cidade = cidade;
+	}
+	
+	/**
+	 * @return O estado(UF) da instituição
+	 */
+	public String getEstado() {
+		return estado;
+	}
+	
+	/**
+	 * @param estado
+	 *            Define o estado(UF) da instituição
+	 */
+	public void setEstado(String estado) {
+		this.estado = estado;
+	}
+	
+	/**
+	 * @return O cep da instituição
+	 */
+	public String getCep() {
+		return cep;
+	}
+	
+	/**
+	 * @param cep
+	 *            Define o cep da instituição
+	 */
+	public void setCep(String cep) {
+		this.cep = cep;
+	}
+	
+	/**
+	 * @return O telefone da instituição
+	 */
+	public String getTelefone() {
+		return telefone;
+	}
+	
+	/**
+	 * @param telefone
+	 *            Define o telefone da instituição
+	 */
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
 	}
 }
