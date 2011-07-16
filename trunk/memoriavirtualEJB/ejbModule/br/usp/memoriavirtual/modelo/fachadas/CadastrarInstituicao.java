12c09b0c18378a40a7c13ea9d331df9f6ce5d60b
joesantos <joesantos@699894a6-35f8-415d-b816-a1f94c44af83>
2011-07-16 21:46:36 +0000

    Criada a classe responsável pelo cadatro da insituição no banco
    git-svn-id: file:///home/brauner/Desktop/DEV_DUMP_19.05.2015/svn_storage/memoria_virtual_src@189 699894a6-35f8-415d-b816-a1f94c44af83

---
+++ trunk/memoriavirtualEJB/ejbModule/br/usp/memoriavirtual/modelo/fachadas/CadastrarInstituicao.java
@@ -0,0 +1,13 @@
+package br.usp.memoriavirtual.modelo.fachadas;
+
+import br.usp.memoriavirtual.modelo.entidades.Instituicao;
+import br.usp.memoriavirtual.modelo.fachadas.remoto.CadastrarInstituicaoRemote;
+
+public class CadastrarInstituicao implements CadastrarInstituicaoRemote{
+	
+	public Instituicao cadastrarInstituicao(String Nome, String Localizacao, String Endereco, String Cidade, String Estado, String Cep, String Telefone){
+		
+	}
+
+
+}
