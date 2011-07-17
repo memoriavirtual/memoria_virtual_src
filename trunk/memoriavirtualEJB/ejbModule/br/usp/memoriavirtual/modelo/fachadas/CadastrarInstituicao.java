1d67d0ca92691c35ee20579bef24b8bcabd05a12
joesantos <joesantos@699894a6-35f8-415d-b816-a1f94c44af83>
2011-07-17 16:38:33 +0000

    tentando terminar a implementação
    git-svn-id: file:///home/brauner/Desktop/DEV_DUMP_19.05.2015/svn_storage/memoria_virtual_src@194 699894a6-35f8-415d-b816-a1f94c44af83

--- trunk/memoriavirtualEJB/ejbModule/br/usp/memoriavirtual/modelo/fachadas/CadastrarInstituicao.java
+++ trunk/memoriavirtualEJB/ejbModule/br/usp/memoriavirtual/modelo/fachadas/CadastrarInstituicao.java
@@ -1,11 +1,23 @@
 package br.usp.memoriavirtual.modelo.fachadas;
 
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+
 import br.usp.memoriavirtual.modelo.entidades.Instituicao;
 import br.usp.memoriavirtual.modelo.fachadas.remoto.CadastrarInstituicaoRemote;
 
+@Stateless(mappedName = "RealizarLogin")
 public class CadastrarInstituicao implements CadastrarInstituicaoRemote{
 	
+	@PersistenceContext(unitName = "memoriavirtual")
+    private EntityManager entityManager;
+	
 	public Instituicao cadastrarInstituicao(String Nome, String Localizacao, String Endereco, String Cidade, String Estado, String Cep, String Telefone){
+		
+		Query query;
+		query = this.entityManager.createQuery("INSERT INTO Instituicao VALUES()");
 		
 	}
 
