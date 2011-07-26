7476fa3b24412507f666e6733f6b7d55dd083c9e
baldacim <baldacim@699894a6-35f8-415d-b816-a1f94c44af83>
2011-07-26 14:17:45 +0000

    git-svn-id: file:///home/brauner/Desktop/DEV_DUMP_19.05.2015/svn_storage/memoria_virtual_src@217 699894a6-35f8-415d-b816-a1f94c44af83

--- trunk/memoriavirtualWeb/src/br/usp/memoriavirtual/controle/CadastrarUsuarioMB.java
+++ trunk/memoriavirtualWeb/src/br/usp/memoriavirtual/controle/CadastrarUsuarioMB.java
@@ -1,19 +1,52 @@
 package br.usp.memoriavirtual.controle;
 
-public class CadastrarUsuarioMB {
+import javax.ejb.EJB;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
 
+import br.usp.memoriavirtual.modelo.fachadas.remoto.CadastrarUsuarioRemote;
+
+
+public class CadastrarUsuarioMB {
 	
+	@EJB
+	private CadastrarUsuarioRemote cadastrarUsuarioEJB;
 	private String email = "";
+	private String nomeCompleto = "";
+	private String telefone = "";
 	private String senha = "";
+	private String confirmacaoSenha = "";
+	private String validacao = "";
 	
-	
-	 public CadastrarUsuarioMB(){
+
+	public CadastrarUsuarioMB(){
 			
 	}
 		
 	public String completarCadastro(){
-			
-		return "true";
+		
+		boolean erro = false;
+		
+		String erroEmail = cadastrarUsuarioEJB.validarEmail(email);
+		if(erroEmail != null){
+			erro = true;
+			FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(erroEmail));
+		}
+		
+		//Validar nome
+		//Validar telefone
+		
+		if(!senha.equals(confirmacaoSenha)){
+			erro = true;
+			FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("Confirmacao de senha não confere."));
+		}
+		
+		if(erro)
+			return "falhou";
+		
+		String resultado = cadastrarUsuarioEJB.completarCadastro(email, nomeCompleto, telefone, senha, validacao);
+	
+		return resultado;
 	}
 		
     public String getEmail() {
@@ -23,9 +56,6 @@ public class CadastrarUsuarioMB {
 	public void setEmail(String email) {
 		this.email = email;
 	}
-
-	
-
     
 	public String getSenha() {
 		return senha;
@@ -33,7 +63,38 @@ public class CadastrarUsuarioMB {
 
 	public void setSenha(String senha) {
 		this.senha = senha;
+	}
+
+	public String getNomeCompleto() {
+		return nomeCompleto;
+	}
+
+	public void setNomeCompleto(String nomeCompleto) {
+		this.nomeCompleto = nomeCompleto;
+	}
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+
+	public String getValidacao() {
+		return validacao;
+	}
+
+	public void setValidacao(String validacao) {
+		this.validacao = validacao;
+	}
+
+	public String getConfirmacaoSenha() {
+		return confirmacaoSenha;
+	}
+
+	public void setConfirmacaoSenha(String confirmacaoSenha) {
+		this.confirmacaoSenha = confirmacaoSenha;
 	}	
 
-	
 }
---
+++ trunk/memoriavirtualWeb/src/br/usp/memoriavirtual/servlet/Cadastro.java
@@ -0,0 +1,53 @@
+package br.usp.memoriavirtual.servlet;
+
+import java.io.IOException;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.NoResultException;
+import javax.persistence.Persistence;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import br.usp.memoriavirtual.modelo.entidades.Usuario;
+
+public class Cadastro extends HttpServlet {
+
+	//@PersistenceContext(unitName = "memoriavirtual")
+    //private EntityManager entityManager;
+	
+	EntityManagerFactory emf = Persistence.createEntityManagerFactory("memoriavirtual"); 
+	EntityManager em = emf.createEntityManager();
+	
+	public void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws IOException, ServletException {
+
+		String validacao = request.getParameter("Validacao");
+		String email = request.getParameter("email");
+		
+		Query query = this.em.createQuery("SELECT u FROM Usuario u WHERE u.id = :usuario");
+		query.setParameter("usuario", validacao);
+
+		Usuario usuarioAutenticado = null;
+
+		try {
+			usuarioAutenticado = (Usuario) query.getSingleResult();
+		} catch (NoResultException e) {
+			usuarioAutenticado = null;
+			e.printStackTrace();
+		}
+		
+		if(usuarioAutenticado != null){
+			response.sendRedirect("cadastro.jsf?validacao="+validacao+"&email="+email);  
+	        //javax.servlet.RequestDispatcher dispatcher = request.getRequestDispatcher("cadastro.jsf?validacao="+validacao+"&email="+email);                          
+	        //dispatcher.forward(request, response);  
+		}else{
+			response.sendRedirect("conviteinvalido.jsf");
+		}
+	}
+}
+
+
