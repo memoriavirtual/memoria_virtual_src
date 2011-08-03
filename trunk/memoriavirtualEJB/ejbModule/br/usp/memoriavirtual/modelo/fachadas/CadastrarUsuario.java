64449f4a1f4bae0b8810b545e71f03b6c60d7956
baldacim <baldacim@699894a6-35f8-415d-b816-a1f94c44af83>
2011-08-03 18:17:41 +0000

    git-svn-id: file:///home/brauner/Desktop/DEV_DUMP_19.05.2015/svn_storage/memoria_virtual_src@223 699894a6-35f8-415d-b816-a1f94c44af83

--- trunk/memoriavirtualEJB/ejbModule/br/usp/memoriavirtual/modelo/fachadas/CadastrarUsuario.java
+++ trunk/memoriavirtualEJB/ejbModule/br/usp/memoriavirtual/modelo/fachadas/CadastrarUsuario.java
@@ -13,59 +13,67 @@ import br.usp.memoriavirtual.modelo.entidades.Usuario;
 import br.usp.memoriavirtual.modelo.fachadas.remoto.CadastrarUsuarioRemote;
 
 @Stateless(mappedName = "CadastrarUsuario")
-public class CadastrarUsuario implements CadastrarUsuarioRemote{
-	
+public class CadastrarUsuario implements CadastrarUsuarioRemote {
+
 	@PersistenceContext(unitName = "memoriavirtual")
-    private EntityManager entityManager;
-	
-	public String completarCadastro(String email, String nomeCompleto, String telefone, String senha, String validacao){
-		
-		/*Busco pelo convite usando o campo ID na tabela*/
+	private EntityManager entityManager;
+
+	public Usuario completarCadastro(String id, String email, String nome,
+			String telefone, String senha, String validacao) {
+
+		/* Busco pelo convite usando o campo ID na tabela */
 		Query query = this.entityManager
-			.createQuery("SELECT u FROM Usuario u WHERE u.id = :usuario");
+				.createQuery("SELECT u FROM Usuario u WHERE u.id = :usuario");
 		query.setParameter("usuario", validacao);
 
 		Usuario usuario = null;
-		
-		/*Atualizo o banco com os dados cadastrados*/
-		try {
-		    usuario =  (Usuario) query.getSingleResult();
-		    usuario.setEmail(email);
-		    usuario.setNomeCompleto(nomeCompleto);
-		    usuario.setSenha(senha);
-		    usuario.setAtivo(true);
-		} catch (NoResultException e) {
-		    usuario = null;
-		    e.printStackTrace();
-		    return "Convite n�o encontrado";
-		}
+		Usuario usuarioCadastrado = new Usuario();
 
-	    
-		return "sucesso";
+		/* 
+		 * Insiro no banco de dados uma nova tupla com os dados cadastrados
+		 * e deleto a tupla usada para enviar o convite
+		 */
+		try {
+			usuario = (Usuario) query.getSingleResult();
+			usuarioCadastrado.setId(id);
+			usuarioCadastrado.setEmail(email);
+			usuarioCadastrado.setNomeCompleto(nome);
+			usuarioCadastrado.setSenha(senha);
+			usuarioCadastrado.setAtivo(true);
+			usuarioCadastrado.setAdministrador(usuario.isAdministrador());
+			entityManager.persist(usuarioCadastrado);
+			entityManager.remove(usuario);
+			return usuarioCadastrado.clone();
+
+		} catch (NoResultException e) {
+			usuario = null;
+			e.printStackTrace();
+			return null;
+		}
 	}
-	
+
 	public String validarEmail(String email) {
 
-			String regexp = "[a-z0-9!#$%&’*+/=?^_‘{|}~-]+(?:\\."
-					+ "[a-z0-9!#$%&’*+/=?^_‘{|}~-]+)*@"
-					+ "(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?";
-			Pattern pattern = Pattern.compile(regexp);
-			Matcher matcher = pattern.matcher(email);
+		String regexp = "[a-z0-9!#$%&’*+/=?^_‘{|}~-]+(?:\\."
+				+ "[a-z0-9!#$%&’*+/=?^_‘{|}~-]+)*@"
+				+ "(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?";
+		Pattern pattern = Pattern.compile(regexp);
+		Matcher matcher = pattern.matcher(email);
 
-			if (!matcher.matches())
-				return "Formato invalido de email";
-			
-			Query query = this.entityManager
-						.createQuery("SELECT u FROM Usuario u WHERE u.email = :email");
-			query.setParameter("email", email);
-				
-			try {
-				query.getSingleResult();
-				return "Email ja cadastrado.";
-			} catch (NoResultException e) {
-				
-			}
-			return null;
+		if (!matcher.matches())
+			return "Formato invalido de email";
+
+		Query query = this.entityManager
+				.createQuery("SELECT u FROM Usuario u WHERE u.email = :email");
+		query.setParameter("email", email);
+
+		try {
+			query.getSingleResult();
+			return "Email ja cadastrado.";
+		} catch (NoResultException e) {
+
+		}
+		return null;
 	}
 
 }
