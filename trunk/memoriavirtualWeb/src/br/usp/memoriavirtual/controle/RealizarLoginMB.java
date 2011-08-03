6e639a338fe29542f401f7f545ad268fc2b69c48
baldacim <baldacim@699894a6-35f8-415d-b816-a1f94c44af83>
2011-08-03 18:22:20 +0000

    git-svn-id: file:///home/brauner/Desktop/DEV_DUMP_19.05.2015/svn_storage/memoria_virtual_src@228 699894a6-35f8-415d-b816-a1f94c44af83

--- trunk/memoriavirtualWeb/src/br/usp/memoriavirtual/controle/RealizarLoginMB.java
+++ trunk/memoriavirtualWeb/src/br/usp/memoriavirtual/controle/RealizarLoginMB.java
@@ -13,82 +13,85 @@ import br.usp.memoriavirtual.modelo.fachadas.remoto.RealizarLoginRemote;
 
 public class RealizarLoginMB {
 
-    @EJB
-    private RealizarLoginRemote realizarLoginEJB;
-    private String usuario = "";
-    private String senha = "";
+	@EJB
+	private RealizarLoginRemote realizarLoginEJB;
+	private String usuario = "";
+	private String senha = "";
 
-    /**
-     * html> <head> <meta http-equiv="Content-Type"
-     * content="text/html; charset=ISO-8859-1"> <title>Logado com
-     * sucesso</title> </head> <body>
-     * 
-     * <h1>Logado com sucesso</h1>
-     * 
-     * </body> </html> Verifica as informacões de usuário e senha na base de
-     * dados.
-     * 
-     * @return <code>true</code> se o usuário foi autenticado com sucesso ou
-     *         <code>false</code> caso contrário.
-     * @throws CloneNotSupportedException
-     * @throws UnknownHostException
-     * @throws SocketException
-     */
-    public String autenticarUsuario() {
-	boolean autenticado = false;
+	/**
+	 * html> <head> <meta http-equiv="Content-Type"
+	 * content="text/html; charset=ISO-8859-1"> <title>Logado com
+	 * sucesso</title> </head> <body>
+	 * 
+	 * <h1>Logado com sucesso</h1>
+	 * 
+	 * </body> </html> Verifica as informacões de usuário e senha na base de
+	 * dados.
+	 * 
+	 * @return <code>true</code> se o usuário foi autenticado com sucesso ou
+	 *         <code>false</code> caso contrário.
+	 * @throws CloneNotSupportedException
+	 * @throws UnknownHostException
+	 * @throws SocketException
+	 */
+	public String autenticarUsuario() {
+		boolean autenticado = false;
 
-	Usuario usuarioAutenticado = null;
-	try {
-	    usuarioAutenticado = realizarLoginEJB.realizarLogin(this.getUsuario(), this.getSenha());
-	} catch (CloneNotSupportedException e) {
-	    e.printStackTrace();
+		Usuario usuarioAutenticado = null;
+		try {
+			usuarioAutenticado = realizarLoginEJB.realizarLogin(
+					this.getUsuario(), this.getSenha());
+		} catch (CloneNotSupportedException e) {
+			e.printStackTrace();
+		}
+
+		if (usuarioAutenticado != null) {
+			autenticado = true;
+			HttpServletRequest request = (HttpServletRequest) FacesContext
+					.getCurrentInstance().getExternalContext().getRequest();
+			request.getSession().setAttribute("usuario", usuarioAutenticado);
+		} else {
+			FacesContext.getCurrentInstance().getExternalContext()
+					.invalidateSession();
+		}
+
+		if (!autenticado)
+			FacesContext.getCurrentInstance().addMessage(null,
+					new FacesMessage("Usuário ou Senha incorretos."));
+
+		this.setSenha(null);
+
+		return autenticado ? "sucesso" : "falha";
 	}
 
-	if (usuarioAutenticado != null) {
-	    autenticado = true;
-	    HttpServletRequest request = (HttpServletRequest) FacesContext.getCurrentInstance().getExternalContext()
-		    .getRequest();
-	    request.getSession().setAttribute("usuario", usuarioAutenticado);
-	} else {
-	    FacesContext.getCurrentInstance().getExternalContext().invalidateSession();
+	/**
+	 * @return the usuario
+	 */
+	public String getUsuario() {
+		return usuario;
 	}
 
-	if (!autenticado)
-	    FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("Usuário ou Senha incorretos."));
+	/**
+	 * @param usuario
+	 *            the usuario to set
+	 */
+	public void setUsuario(String usuario) {
+		this.usuario = usuario;
+	}
 
-	this.setSenha(null);
+	/**
+	 * @return the senha
+	 */
+	public String getSenha() {
+		return senha;
+	}
 
-	return autenticado ? "sucesso" : "falha";
-    }
-
-    /**
-     * @return the usuario
-     */
-    public String getUsuario() {
-	return usuario;
-    }
-
-    /**
-     * @param usuario
-     *            the usuario to set
-     */
-    public void setUsuario(String usuario) {
-	this.usuario = usuario;
-    }
-
-    /**
-     * @return the senha
-     */
-    public String getSenha() {
-	return senha;
-    }
-
-    /**
-     * @param senha
-     *            the senha to set
-     */
-    public void setSenha(String senha) {
-	this.senha = senha;
-    }
+	/**
+	 * @param senha
+	 *            the senha to set
+	 */
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
 
 }
