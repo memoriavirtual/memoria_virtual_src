1cd318f8817236fc3aa3ba1d1d356919c8a5a4a8
tbianchi <tbianchi@699894a6-35f8-415d-b816-a1f94c44af83>
2011-08-18 15:05:24 +0000

    Nunca deixa sem tratar exceções e utilize nome de variáveis que sejam fáceis de entender.
    git-svn-id: file:///home/brauner/Desktop/DEV_DUMP_19.05.2015/svn_storage/memoria_virtual_src@273 699894a6-35f8-415d-b816-a1f94c44af83

--- trunk/memoriavirtualEJB/ejbModule/br/usp/memoriavirtual/modelo/fachadas/CadastrarInstituicao.java
+++ trunk/memoriavirtualEJB/ejbModule/br/usp/memoriavirtual/modelo/fachadas/CadastrarInstituicao.java
@@ -8,30 +8,31 @@ import br.usp.memoriavirtual.modelo.entidades.Instituicao;
 import br.usp.memoriavirtual.modelo.fachadas.remoto.CadastrarInstituicaoRemote;
 
 @Stateless(mappedName = "CadastrarInstituicao")
-public class CadastrarInstituicao implements CadastrarInstituicaoRemote{
-	
-	@PersistenceContext(unitName = "memoriavirtual")
-    private EntityManager entityManager;
-	
-	public Instituicao cadastrarInstituicao(String Nome, String Localizacao, String Endereco, String Cidade, String Estado, String Cep, String Telefone){
-		
-		Instituicao ins = new Instituicao(Nome, Localizacao, Endereco, Cidade, Estado, Cep, Telefone);
+public class CadastrarInstituicao implements CadastrarInstituicaoRemote {
 
-		//verifica se ja existe uma instituicao com esse nome
-		if(entityManager.find(ins.getClass(), ins.getNome()) != null)
-			return null;
-		//tenta persistir no banco
-		try{
-			entityManager.persist(ins);
-		}
-		catch(Exception e){
-			return null;
-		}
-		
-		//certifica de que a persistencia ocorreu com sucesso
-		if(!entityManager.contains(ins))
-			return null;
-		
-		return ins;
+    @PersistenceContext(unitName = "memoriavirtual")
+    private EntityManager entityManager;
+
+    public Instituicao cadastrarInstituicao(String Nome, String Localizacao, String Endereco, String Cidade,
+	    String Estado, String Cep, String Telefone) {
+
+	Instituicao instituicao = new Instituicao(Nome, Localizacao, Endereco, Cidade, Estado, Cep, Telefone);
+
+	// verifica se ja existe uma instituicao com esse nome
+	if (entityManager.find(instituicao.getClass(), instituicao.getNome()) != null)
+	    return null;
+	// tenta persistir no banco
+	try {
+	    entityManager.persist(instituicao);
+	} catch (Exception e) {
+	    e.printStackTrace();
+	    return null;
 	}
+
+	// certifica que a persistencia ocorreu com sucesso
+	if (!entityManager.contains(instituicao))
+	    return null;
+
+	return instituicao;
+    }
 }
