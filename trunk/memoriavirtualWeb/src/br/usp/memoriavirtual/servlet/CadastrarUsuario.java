b728f5e6abce3fa643ab92d7897d851d1977d678
diegominatel <diegominatel@699894a6-35f8-415d-b816-a1f94c44af83>
2011-09-20 18:30:23 +0000

    git-svn-id: file:///home/brauner/Desktop/DEV_DUMP_19.05.2015/svn_storage/memoria_virtual_src@307 699894a6-35f8-415d-b816-a1f94c44af83

--- trunk/memoriavirtualWeb/src/br/usp/memoriavirtual/servlet/CadastrarUsuario.java
+++ trunk/memoriavirtualWeb/src/br/usp/memoriavirtual/servlet/CadastrarUsuario.java
@@ -16,44 +16,49 @@ import br.usp.memoriavirtual.modelo.entidades.Usuario;
 
 public class CadastrarUsuario extends HttpServlet {
 
-    /**
-     * Serial Version UID
-     */
-    private static final long serialVersionUID = 2734229838622329992L;
-    EntityManagerFactory emf = Persistence.createEntityManagerFactory("memoriavirtual");
-    EntityManager em = emf.createEntityManager();
+	/**
+	 * Serial Version UID
+	 */
+	private static final long serialVersionUID = 2734229838622329992L;
+	EntityManagerFactory emf = Persistence
+			.createEntityManagerFactory("memoriavirtual");
+	EntityManager em = emf.createEntityManager();
 
-    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+	public void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws IOException, ServletException {
 
-	String validacao = request.getParameter("Validacao");
-	String email = request.getParameter("email");
+		String validacao = request.getParameter("Validacao");
+		String email = request.getParameter("email");
 
-	Query query = this.em.createQuery("SELECT u FROM Usuario u WHERE u.id = :usuario");
-	query.setParameter("usuario", validacao);
+		Query query = this.em
+				.createQuery("SELECT u FROM Usuario u WHERE u.id = :usuario");
+		query.setParameter("usuario", validacao);
 
-	Usuario usuarioAutenticado = null;
+		Usuario usuarioAutenticado = null;
 
-	try {
-	    usuarioAutenticado = (Usuario) query.getSingleResult();
-	    if (!usuarioAutenticado.isAtivo()) {
-		request.getSession().setAttribute("usuario", usuarioAutenticado);
-	    } else {
-		usuarioAutenticado = null;
-	    }
-	} catch (NoResultException e) {
-	    usuarioAutenticado = null;
-	    e.printStackTrace();
+		try {
+			usuarioAutenticado = (Usuario) query.getSingleResult();
+			if (!usuarioAutenticado.isAtivo()) {
+				request.getSession()
+						.setAttribute("usuario", usuarioAutenticado);
+			} else {
+				usuarioAutenticado = null;
+			}
+		} catch (NoResultException e) {
+			usuarioAutenticado = null;
+			e.printStackTrace();
+		}
+
+		if (usuarioAutenticado != null) {
+			response.sendRedirect("restrito/cadastrarusuario.jsf?validacao="
+					+ validacao + "&email=" + email);
+			// Caso queria fazer por foward para n�o mudar a url usar c�digo
+			// abaixo:
+			// javax.servlet.RequestDispatcher dispatcher =
+			// request.getRequestDispatcher("cadastrarusuario.jsf?validacao="+validacao+"&email="+email);
+			// dispatcher.forward(request, response);
+		} else {
+			response.sendRedirect("conviteinvalido.jsf");
+		}
 	}
-
-	if (usuarioAutenticado != null) {
-	    response.sendRedirect("restrito/cadastrarusuario.jsf?validacao=" + validacao + "&email=" + email);
-	    // Caso queria fazer por foward para n�o mudar a url usar c�digo
-	    // abaixo:
-	    // javax.servlet.RequestDispatcher dispatcher =
-	    // request.getRequestDispatcher("cadastrarusuario.jsf?validacao="+validacao+"&email="+email);
-	    // dispatcher.forward(request, response);
-	} else {
-	    response.sendRedirect("conviteinvalido.jsf");
-	}
-    }
 }
