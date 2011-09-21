bb3e24504fdeef7fb316e6bf6996dab98d95c2c2
diegominatel <diegominatel@699894a6-35f8-415d-b816-a1f94c44af83>
2011-09-21 17:01:59 +0000

    Fase de Construção ainda
    git-svn-id: file:///home/brauner/Desktop/DEV_DUMP_19.05.2015/svn_storage/memoria_virtual_src@318 699894a6-35f8-415d-b816-a1f94c44af83

---
+++ trunk/memoriavirtualEJB/ejbModule/br/usp/memoriavirtual/modelo/fachadas/EditarCadastroProprio.java
@@ -0,0 +1,28 @@
+package br.usp.memoriavirtual.modelo.fachadas;
+
+import javax.ejb.Stateless;
+
+import br.usp.memoriavirtual.modelo.entidades.Usuario;
+
+import br.usp.memoriavirtual.modelo.fachadas.remoto.EditarCadastroProprioRemote;
+
+@Stateless(mappedName = "EditarCadastroProprio")
+public class EditarCadastroProprio implements EditarCadastroProprioRemote {
+
+	private Usuario usuario;
+	
+	public Usuario atualizarDadosUsuario(String id, String email, String nome,
+			String telefone, String senha, String validacao){
+		return usuario;
+	}
+	
+	public boolean disponibilidadeEmail(String email){
+		return true;
+	}
+	
+	public Usuario recuperarDadosUsuario(String id){
+		return usuario;
+	}
+
+
+}
