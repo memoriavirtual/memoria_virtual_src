da4fcdf3f385d9e627855595bda2a34cbf1fc760
diegominatel <diegominatel@699894a6-35f8-415d-b816-a1f94c44af83>
2011-09-21 13:52:02 +0000

    git-svn-id: file:///home/brauner/Desktop/DEV_DUMP_19.05.2015/svn_storage/memoria_virtual_src@310 699894a6-35f8-415d-b816-a1f94c44af83

--- trunk/memoriavirtualEJB/ejbModule/br/usp/memoriavirtual/modelo/entidades/Usuario.java
+++ trunk/memoriavirtualEJB/ejbModule/br/usp/memoriavirtual/modelo/entidades/Usuario.java
@@ -19,160 +19,163 @@ import javax.validation.constraints.Pattern;
 @Entity
 public class Usuario implements Serializable, Cloneable {
 
-    /**
-     * Serial Version UID
-     */
-    private static final long serialVersionUID = -2966677929188737645L;
-    @Id
-    private String id;
-    @NotNull
-    @Column(unique = true)
-    @Pattern(regexp = "[a-z0-9!#$%&’*+/=?^_‘{|}~-]+(?:\\." + "[a-z0-9!#$%&’*+/=?^_‘{|}~-]+)*@"
-	    + "(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?", message = "Email invalido")
-    private String email;
-    private String nomeCompleto;
-    private String senha;
-    private Date validade;
-    @NotNull
-    private Boolean administrador;
-    @NotNull
-    private Boolean ativo;
+	/**
+	 * Serial Version UID
+	 */
+	private static final long serialVersionUID = -2966677929188737645L;
+	@Id
+	private String id;
+	@NotNull
+	@Column(unique = true)
+	@Pattern(regexp = "[a-z0-9!#$%&’*+/=?^_‘{|}~-]+(?:\\."
+			+ "[a-z0-9!#$%&’*+/=?^_‘{|}~-]+)*@"
+			+ "(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?", message = "Email invalido")
+	private String email;
+	private String nomeCompleto;
+	private String senha;
+	private Date validade;
+	@NotNull
+	private Boolean administrador;
+	@NotNull
+	private Boolean ativo;
 
-    /**
-     * Construtor padrão
-     */
-    public Usuario() {
-	super();
-	System.out.println();
-    }
-
-    /**
-     * @return the login
-     */
-    public String getId() {
-	return id;
-    }
-
-    /**
-     * @param login
-     *            the login to set
-     */
-    public void setId(String id) {
-	this.id = id;
-    }
-
-    /**
-     * @return the senha
-     */
-    public String getSenha() {
-	return senha;
-    }
-
-    /**
-     * @param senha
-     *            the senha to set
-     */
-    public void setSenha(String senha) {
-	String senhaCriptografada = gerarHash(senha);
-	this.senha = senhaCriptografada;
-    }
-
-    public String getEmail() {
-	return email;
-    }
-
-    public void setEmail(String email) {
-	this.email = email;
-    }
-
-    /**
-     * @return validade do cadastro
-     */
-    public Date getValidade() {
-	return validade;
-    }
-
-    /**
-     * @param validade
-     *            a data limite da validade
-     */
-    public void setValidade(java.util.Date validade) {
-	java.sql.Date temp = new Date(validade.getTime());
-	this.validade = temp;
-    }
-
-    public Boolean isAdministrador() {
-	return administrador;
-    }
-
-    /**
-     * @param administrador
-     *            se o usuario é ou não administrador do sistema
-     */
-
-    public void setAdministrador(Boolean administrador) {
-	this.administrador = administrador;
-    }
-
-    public Boolean isAtivo() {
-	return ativo;
-    }
-
-    /**
-     * @param ativo
-     *            Se o usuario ja fez cadastro
-     */
-
-    public void setAtivo(Boolean ativo) {
-	this.ativo = ativo;
-    }
-
-    public String getNomeCompleto() {
-	return nomeCompleto;
-    }
-
-    public void setNomeCompleto(String nomeCompleto) {
-	this.nomeCompleto = nomeCompleto;
-    }
-
-    /**
-     * @param input
-     *            a senha para ser criptografada ou o email para criar o id
-     *            usando no convite
-     */
-    public static String gerarHash(String input) {
-
-	MessageDigest md = null;
-	byte messageDigest[] = null;
-	try {
-	    md = MessageDigest.getInstance("SHA-512");
-	    messageDigest = md.digest(input.getBytes("UTF-8"));
-	} catch (NoSuchAlgorithmException ex) {
-	    Logger.getLogger(Usuario.class.getName()).log(Level.SEVERE, null, ex);
-	} catch (UnsupportedEncodingException ex) {
-	    Logger.getLogger(Usuario.class.getName()).log(Level.SEVERE, null, ex);
-	} catch (NullPointerException ex) {
-	    return null;
+	/**
+	 * Construtor padrão
+	 */
+	public Usuario() {
+		super();
+		System.out.println();
 	}
-	// Converte para hexa antes de utilizar a senha criptografada
-	StringBuilder hexString = new StringBuilder();
-	for (byte b : messageDigest) {
-	    hexString.append(String.format("%02X", 0xFF & b));
-	}
-	String inputCifrado = hexString.toString();
-	return inputCifrado;
-    }
 
-    @Override
-    public Usuario clone() {
-
-	Usuario clone = null;
-	try {
-	    clone = (Usuario) super.clone();
-	    clone.setSenha(null);
-	} catch (CloneNotSupportedException e) {
-	    e.printStackTrace();
+	/**
+	 * @return the login
+	 */
+	public String getId() {
+		return id;
 	}
-	return clone;
-    }
+
+	/**
+	 * @param login
+	 *            the login to set
+	 */
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	/**
+	 * @return the senha
+	 */
+	public String getSenha() {
+		return senha;
+	}
+
+	/**
+	 * @param senha
+	 *            the senha to set
+	 */
+	public void setSenha(String senha) {
+		String senhaCriptografada = gerarHash(senha);
+		this.senha = senhaCriptografada;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	/**
+	 * @return validade do cadastro
+	 */
+	public Date getValidade() {
+		return validade;
+	}
+
+	/**
+	 * @param validade
+	 *            a data limite da validade
+	 */
+	public void setValidade(java.util.Date validade) {
+		java.sql.Date temp = new Date(validade.getTime());
+		this.validade = temp;
+	}
+
+	public Boolean isAdministrador() {
+		return administrador;
+	}
+
+	/**
+	 * @param administrador
+	 *            se o usuario é ou não administrador do sistema
+	 */
+
+	public void setAdministrador(Boolean administrador) {
+		this.administrador = administrador;
+	}
+
+	public Boolean isAtivo() {
+		return ativo;
+	}
+
+	/**
+	 * @param ativo
+	 *            Se o usuario ja fez cadastro
+	 */
+
+	public void setAtivo(Boolean ativo) {
+		this.ativo = ativo;
+	}
+
+	public String getNomeCompleto() {
+		return nomeCompleto;
+	}
+
+	public void setNomeCompleto(String nomeCompleto) {
+		this.nomeCompleto = nomeCompleto;
+	}
+
+	/**
+	 * @param input
+	 *            a senha para ser criptografada ou o email para criar o id
+	 *            usando no convite
+	 */
+	public static String gerarHash(String input) {
+
+		MessageDigest md = null;
+		byte messageDigest[] = null;
+		try {
+			md = MessageDigest.getInstance("SHA-512");
+			messageDigest = md.digest(input.getBytes("UTF-8"));
+		} catch (NoSuchAlgorithmException ex) {
+			Logger.getLogger(Usuario.class.getName()).log(Level.SEVERE, null,
+					ex);
+		} catch (UnsupportedEncodingException ex) {
+			Logger.getLogger(Usuario.class.getName()).log(Level.SEVERE, null,
+					ex);
+		} catch (NullPointerException ex) {
+			return null;
+		}
+		// Converte para hexa antes de utilizar a senha criptografada
+		StringBuilder hexString = new StringBuilder();
+		for (byte b : messageDigest) {
+			hexString.append(String.format("%02X", 0xFF & b));
+		}
+		String inputCifrado = hexString.toString();
+		return inputCifrado;
+	}
+
+	@Override
+	public Usuario clone() {
+
+		Usuario clone = null;
+		try {
+			clone = (Usuario) super.clone();
+			clone.setSenha(null);
+		} catch (CloneNotSupportedException e) {
+			e.printStackTrace();
+		}
+		return clone;
+	}
 }
