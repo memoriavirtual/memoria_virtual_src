dc542b6dc85be6a3a01d3fb2575f74d1a9e69dd0
diegominatel <diegominatel@699894a6-35f8-415d-b816-a1f94c44af83>
2011-09-21 14:44:20 +0000

    Inclusão do método validarEmail
    git-svn-id: file:///home/brauner/Desktop/DEV_DUMP_19.05.2015/svn_storage/memoria_virtual_src@314 699894a6-35f8-415d-b816-a1f94c44af83

--- trunk/memoriavirtualEJB/ejbModule/br/usp/memoriavirtual/modelo/fachadas/MemoriaVirtual.java
+++ trunk/memoriavirtualEJB/ejbModule/br/usp/memoriavirtual/modelo/fachadas/MemoriaVirtual.java
@@ -4,8 +4,11 @@ import java.io.IOException;
 import java.net.InetAddress;
 import java.net.NetworkInterface;
 import java.util.Enumeration;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import javax.ejb.Singleton;
+
 
 import br.usp.memoriavirtual.modelo.fachadas.remoto.MemoriaVirtualRemote;
 
@@ -15,41 +18,63 @@ import br.usp.memoriavirtual.modelo.fachadas.remoto.MemoriaVirtualRemote;
 @Singleton
 public class MemoriaVirtual implements MemoriaVirtualRemote {
 
-    private static InetAddress enderecoServidor = null;
+	private static InetAddress enderecoServidor = null;
+	
 
-    /**
-     * Default constructor.
-     */
-    public MemoriaVirtual() {
 
-    }
+	/**
+	 * Default constructor.
+	 */
+	public MemoriaVirtual() {
 
-    /**
-     * Retorna o endereço físico do servidor.
-     * @throws IOException
-     * 
-     */
-    public InetAddress getEnderecoServidor() throws IOException {
-
-	if (enderecoServidor == null) {
-	    Enumeration<NetworkInterface> interfaces = NetworkInterface.getNetworkInterfaces();
-
-	    while (interfaces.hasMoreElements()) {
-		NetworkInterface interfaceRede = (NetworkInterface) interfaces.nextElement();
-		if (interfaceRede.isUp()) {
-		    Enumeration<InetAddress> enderecosRede = interfaceRede.getInetAddresses();
-		    while (enderecosRede.hasMoreElements()) {
-			InetAddress enderecoRede = (InetAddress) enderecosRede.nextElement();
-			if (enderecoRede.isReachable(10) && !enderecoRede.isLoopbackAddress()
-				&& !enderecoRede.isAnyLocalAddress()) {
-			    enderecoServidor = enderecoRede;
-			    return enderecoServidor;
-			}
-		    }
-		}
-	    }
 	}
-	return enderecoServidor;
-    }
+
+	/**
+	 * Retorna o endereço físico do servidor.
+	 * 
+	 * @throws IOException
+	 * 
+	 */
+	public InetAddress getEnderecoServidor() throws IOException {
+
+		if (enderecoServidor == null) {
+			Enumeration<NetworkInterface> interfaces = NetworkInterface
+					.getNetworkInterfaces();
+
+			while (interfaces.hasMoreElements()) {
+				NetworkInterface interfaceRede = (NetworkInterface) interfaces
+						.nextElement();
+				if (interfaceRede.isUp()) {
+					Enumeration<InetAddress> enderecosRede = interfaceRede
+							.getInetAddresses();
+					while (enderecosRede.hasMoreElements()) {
+						InetAddress enderecoRede = (InetAddress) enderecosRede
+								.nextElement();
+						if (enderecoRede.isReachable(10)
+								&& !enderecoRede.isLoopbackAddress()
+								&& !enderecoRede.isAnyLocalAddress()) {
+							enderecoServidor = enderecoRede;
+							return enderecoServidor;
+						}
+					}
+				}
+			}
+		}
+		return enderecoServidor;
+	}
+
+	public boolean validarEmail(String email) {
+
+		String regexp = "[a-z0-9!#$%&’*+/=?^_‘{|}~-]+(?:\\."
+				+ "[a-z0-9!#$%&’*+/=?^_‘{|}~-]+)*@"
+				+ "(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?";
+		Pattern pattern = Pattern.compile(regexp);
+		Matcher matcher = pattern.matcher(email);
+
+		if (!matcher.matches())
+			return false;
+
+		return true;
+	}
 
 }
