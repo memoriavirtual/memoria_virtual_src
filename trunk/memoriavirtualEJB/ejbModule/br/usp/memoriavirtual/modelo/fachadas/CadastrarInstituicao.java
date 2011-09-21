caab1ae9c645cb823d2b6ac6f29922878142c712
diegominatel <diegominatel@699894a6-35f8-415d-b816-a1f94c44af83>
2011-09-21 12:54:39 +0000

    git-svn-id: file:///home/brauner/Desktop/DEV_DUMP_19.05.2015/svn_storage/memoria_virtual_src@308 699894a6-35f8-415d-b816-a1f94c44af83

--- trunk/memoriavirtualEJB/ejbModule/br/usp/memoriavirtual/modelo/fachadas/CadastrarInstituicao.java
+++ trunk/memoriavirtualEJB/ejbModule/br/usp/memoriavirtual/modelo/fachadas/CadastrarInstituicao.java
@@ -10,29 +10,31 @@ import br.usp.memoriavirtual.modelo.fachadas.remoto.CadastrarInstituicaoRemote;
 @Stateless(mappedName = "CadastrarInstituicao")
 public class CadastrarInstituicao implements CadastrarInstituicaoRemote {
 
-    @PersistenceContext(unitName = "memoriavirtual")
-    private EntityManager entityManager;
+	@PersistenceContext(unitName = "memoriavirtual")
+	private EntityManager entityManager;
 
-    public Instituicao cadastrarInstituicao(String Nome, String Localizacao, String Endereco, String Cidade,
-	    String Estado, String Cep, String Telefone) {
+	public Instituicao cadastrarInstituicao(String Nome, String Localizacao,
+			String Endereco, String Cidade, String Estado, String Cep,
+			String Telefone) {
 
-	Instituicao instituicao = new Instituicao(Nome, Localizacao, Endereco, Cidade, Estado, Cep, Telefone);
+		Instituicao instituicao = new Instituicao(Nome, Localizacao, Endereco,
+				Cidade, Estado, Cep, Telefone);
 
-	// verifica se ja existe uma instituicao com esse nome
-	if (entityManager.find(instituicao.getClass(), instituicao.getNome()) != null)
-	    return null;
-	// tenta persistir no banco
-	try {
-	    entityManager.persist(instituicao);
-	} catch (Exception e) {
-	    e.printStackTrace();
-	    return null;
+		// verifica se ja existe uma instituicao com esse nome
+		if (entityManager.find(instituicao.getClass(), instituicao.getNome()) != null)
+			return null;
+		// tenta persistir no banco
+		try {
+			entityManager.persist(instituicao);
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}
+
+		// certifica que a persistencia ocorreu com sucesso
+		if (!entityManager.contains(instituicao))
+			return null;
+
+		return instituicao;
 	}
-
-	// certifica que a persistencia ocorreu com sucesso
-	if (!entityManager.contains(instituicao))
-	    return null;
-
-	return instituicao;
-    }
 }
