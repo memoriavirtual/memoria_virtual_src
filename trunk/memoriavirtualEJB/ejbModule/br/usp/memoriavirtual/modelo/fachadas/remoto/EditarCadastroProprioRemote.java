8f8eaa495835b0731dae55c6abb924a0b51bbdf3
diegominatel <diegominatel@699894a6-35f8-415d-b816-a1f94c44af83>
2011-09-21 14:47:24 +0000

    git-svn-id: file:///home/brauner/Desktop/DEV_DUMP_19.05.2015/svn_storage/memoria_virtual_src@316 699894a6-35f8-415d-b816-a1f94c44af83

---
+++ trunk/memoriavirtualEJB/ejbModule/br/usp/memoriavirtual/modelo/fachadas/remoto/EditarCadastroProprioRemote.java
@@ -0,0 +1,10 @@
+package br.usp.memoriavirtual.modelo.fachadas.remoto;
+
+import javax.ejb.Remote;
+
+import br.usp.memoriavirtual.modelo.entidades.Usuario;
+
+@Remote
+public interface EditarCadastroProprioRemote {
+
+}
