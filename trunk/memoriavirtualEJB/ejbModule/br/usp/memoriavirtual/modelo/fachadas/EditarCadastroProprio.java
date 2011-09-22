ecf14c8834de92d57949ac091d49c556aa6f875b
diegominatel <diegominatel@699894a6-35f8-415d-b816-a1f94c44af83>
2011-09-22 15:01:22 +0000

    git-svn-id: file:///home/brauner/Desktop/DEV_DUMP_19.05.2015/svn_storage/memoria_virtual_src@328 699894a6-35f8-415d-b816-a1f94c44af83

--- trunk/memoriavirtualEJB/ejbModule/br/usp/memoriavirtual/modelo/fachadas/EditarCadastroProprio.java
+++ trunk/memoriavirtualEJB/ejbModule/br/usp/memoriavirtual/modelo/fachadas/EditarCadastroProprio.java
@@ -1,6 +1,8 @@
 package br.usp.memoriavirtual.modelo.fachadas;
 
 import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 
 import br.usp.memoriavirtual.modelo.entidades.Usuario;
 
@@ -9,10 +11,24 @@ import br.usp.memoriavirtual.modelo.fachadas.remoto.EditarCadastroProprioRemote;
 @Stateless(mappedName = "EditarCadastroProprio")
 public class EditarCadastroProprio implements EditarCadastroProprioRemote {
 
+	@PersistenceContext(unitName = "memoriavirtual")
+	private EntityManager entityManager;
+
 	private Usuario usuario;
 
-	public void atualizarDadosUsuario(String id, String email, String nome,
-			String telefone, String senha, String validacao) {
+	public void atualizarDadosUsuario(String id, String novoEmail,
+			String novoNomeCompleto, String novoTelefone, String novaSenha,
+			String validacao) throws ModeloException {
+
+		usuario = this.entityManager.find(Usuario.class, id);
+		if (usuario != null) {
+			usuario.setEmail(novoEmail);
+			usuario.setNomeCompleto(novoNomeCompleto);
+			usuario.setSenha(novaSenha);
+			entityManager.flush();
+		}
+		else
+			throw new ModeloException("Usuario não encontrado");
 	}
 
 	public boolean disponibilidadeEmail(String email) {
