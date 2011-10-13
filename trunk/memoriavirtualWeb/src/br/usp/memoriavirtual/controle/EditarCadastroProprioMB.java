0798630959ed5acf1179c8151e44378212536d47
diegominatel <diegominatel@699894a6-35f8-415d-b816-a1f94c44af83>
2011-10-13 20:06:17 +0000

    git-svn-id: file:///home/brauner/Desktop/DEV_DUMP_19.05.2015/svn_storage/memoria_virtual_src@352 699894a6-35f8-415d-b816-a1f94c44af83

--- trunk/memoriavirtualWeb/src/br/usp/memoriavirtual/controle/EditarCadastroProprioMB.java
+++ trunk/memoriavirtualWeb/src/br/usp/memoriavirtual/controle/EditarCadastroProprioMB.java
@@ -3,14 +3,31 @@ package br.usp.memoriavirtual.controle;
 import javax.ejb.EJB;
 
 import br.usp.memoriavirtual.modelo.entidades.Usuario;
+import br.usp.memoriavirtual.modelo.fachadas.remoto.EditarCadastroProprioRemote;
 
 public class EditarCadastroProprioMB {
 
 	@EJB
+	private EditarCadastroProprioRemote editarCadastroProprioEJB;
 	private Usuario usuario;
 	private String novoEmail;
 	private String novoNomeCompleto;
 	private String novaSenha;
+
+	public String editarCadastroProprio() {
+		if (this.novoEmail == null || this.novoNomeCompleto == null
+				|| this.novaSenha == null)
+			return "Incompleto";
+		usuario = this.editarCadastroProprioEJB.recuperarDadosUsuario("1");
+		try {
+			this.editarCadastroProprioEJB.atualizarDadosUsuario(
+					usuario.getId(), this.novoEmail, this.novoNomeCompleto,
+					this.novaSenha);
+			return "Sucesso";
+		} catch (Exception e) {
+			return "Falha";
+		}
+	}
 
 	public String getNomeCompleto() {
 		return this.usuario.getNomeCompleto();
