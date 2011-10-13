f78241758daf2fc51ab9cd5c8c95f3d5d21b70d2
diegominatel <diegominatel@699894a6-35f8-415d-b816-a1f94c44af83>
2011-10-13 20:06:33 +0000

    git-svn-id: file:///home/brauner/Desktop/DEV_DUMP_19.05.2015/svn_storage/memoria_virtual_src@353 699894a6-35f8-415d-b816-a1f94c44af83

--- trunk/memoriavirtualEJB/ejbModule/br/usp/memoriavirtual/modelo/fachadas/remoto/EditarCadastroProprioRemote.java
+++ trunk/memoriavirtualEJB/ejbModule/br/usp/memoriavirtual/modelo/fachadas/remoto/EditarCadastroProprioRemote.java
@@ -8,10 +8,10 @@ import br.usp.memoriavirtual.modelo.fachadas.ModeloException;
 @Remote
 public interface EditarCadastroProprioRemote {
 
-	public void atualizarDadosUsuario(String id, String novoEmail, String novoNomeCompleto,
-			String novoTelefone, String novaSenha, String validacao) throws ModeloException;
-	
-	
+	public void atualizarDadosUsuario(String id, String novoEmail,
+			String novoNomeCompleto, String novaSenha)
+			throws ModeloException;
+
 	public Usuario recuperarDadosUsuario(String id);
-	
+
 }
