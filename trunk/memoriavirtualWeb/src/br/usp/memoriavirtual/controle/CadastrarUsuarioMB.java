de63e736c835bc15efc398a047ddb15679abd75f
baldacim <baldacim@699894a6-35f8-415d-b816-a1f94c44af83>
2011-10-19 21:01:16 +0000

    git-svn-id: file:///home/brauner/Desktop/DEV_DUMP_19.05.2015/svn_storage/memoria_virtual_src@376 699894a6-35f8-415d-b816-a1f94c44af83

--- trunk/memoriavirtualWeb/src/br/usp/memoriavirtual/controle/CadastrarUsuarioMB.java
+++ trunk/memoriavirtualWeb/src/br/usp/memoriavirtual/controle/CadastrarUsuarioMB.java
@@ -1,16 +1,16 @@
 package br.usp.memoriavirtual.controle;
 
 import javax.ejb.EJB;
-import javax.faces.application.FacesMessage;
-import javax.faces.component.UIComponent;
 import javax.faces.context.FacesContext;
 import javax.faces.event.AjaxBehaviorEvent;
 import javax.servlet.http.HttpServletRequest;
 
 import br.usp.memoriavirtual.modelo.entidades.Usuario;
+import br.usp.memoriavirtual.modelo.fachadas.ModeloException;
 import br.usp.memoriavirtual.modelo.fachadas.remoto.CadastrarUsuarioRemote;
 import br.usp.memoriavirtual.modelo.fachadas.remoto.MemoriaVirtualRemote;
-import br.usp.memoriavirtual.utils.MensagensErro;
+import br.usp.memoriavirtual.utils.MensagensDeErro;
+import br.usp.memoriavirtual.utils.ValidacoesDeCampos;
 
 public class CadastrarUsuarioMB {
 
@@ -32,37 +32,62 @@ public class CadastrarUsuarioMB {
 
 	public String completarCadastro() {
 
-		/*FacesContext.getCurrentInstance().addMessage(
-				"resultado",
-				new FacesMessage(FacesMessage.SEVERITY_INFO,
-						"Cadastro concluido com sucesso.", null));
-*/
-		FacesContext.getCurrentInstance().getMessages();
-		MensagensErro.getSucessMessage("cadastro_concluido", "resultado");
-		
-
 		/*
-		 * Realiza o cadastro do usuario no banco de dados e retorna uma cópia
-		 * do usuário sem conter a senha para colocar na seção.
+		 * FacesContext.getCurrentInstance().addMessage( "resultado", new
+		 * FacesMessage(FacesMessage.SEVERITY_INFO,
+		 * "Cadastro concluido com sucesso.", null));
 		 */
-		
-		/*
-		Usuario usuario = cadastrarUsuarioEJB.completarCadastro(id, email,
-				nomeCompleto, telefone, senha, validacao);
-		if (usuario != null) {
-			HttpServletRequest request = (HttpServletRequest) FacesContext
-					.getCurrentInstance().getExternalContext().getRequest();
-			request.getSession().setAttribute("usuario", usuario);
 
-			FacesContext.getCurrentInstance().addMessage(
-					"cadastroConcluido",
-					new FacesMessage(FacesMessage.SEVERITY_INFO,
-							"Cadastro concluido com sucesso.", null));
-		} else {
-			return "falhou";
+		this.validateId();
+		this.validateEmail();
+		this.validateNomeCompleto();
+		this.validateTelefone();
+		this.validateSenha();
+		this.validateConfirmacaoSenha();
+
+		Usuario usuario = new Usuario(this.id, this.email, this.nomeCompleto,
+				this.telefone, this.senha);
+
+		if (!FacesContext.getCurrentInstance().getMessages().hasNext()) {
+
+			try {
+				cadastrarUsuarioEJB.completarCadastro(usuario, validacao);
+			} catch (ModeloException e) {
+				usuario = null;
+				MensagensDeErro
+						.getErrorMessage("convite_invalido", "resultado");
+			} catch (RuntimeException e) {
+				usuario = null;
+				MensagensDeErro.getErrorMessage("erro_cadastramento",
+						"resultado");
+			}
+			if (usuario != null) {
+				usuario = usuario.clone(); //Teste se isso nao altera o usuario ja persistido na banco
+				HttpServletRequest request = (HttpServletRequest) FacesContext
+						.getCurrentInstance().getExternalContext().getRequest();
+				request.getSession().setAttribute("usuario", usuario);
+			}
+
+			this.id = "";
+			this.email = "";
+			this.nomeCompleto = "";
+			this.telefone = "";
+			this.senha = "";
+			this.confirmacaoSenha = "";
+			this.validacao = "";
+
+			MensagensDeErro.getSucessMessage("cadastro_concluido", "resultado");
+
 		}
-*/
 		return "falhou";
+	}
+
+	public String getValidacao() {
+		return validacao;
+	}
+
+	public void setValidacao(String validacao) {
+		this.validacao = validacao;
 	}
 
 	public String getId() {
@@ -74,20 +99,22 @@ public class CadastrarUsuarioMB {
 	}
 
 	public void validateId(AjaxBehaviorEvent event) {
-		System.out.println(event.getComponent().toString());
-		System.out.println(event.getBehavior().toString());
-		
+		this.validateId();
+	}
+
+	public void validateId() {
+
 		if (this.id.equals("")) {
 			String[] argumentos = { "id" };
-			MensagensErro.getErrorMessage("campo_vazio", argumentos,
+			MensagensDeErro.getErrorMessage("campo_vazio", argumentos,
 					"validacaoId");
 		} else if (this.id.length() < 4) {
 			String[] argumentos = { "id", "id_minimo" };
-			MensagensErro.getErrorMessage("tamanho_minimo", argumentos,
+			MensagensDeErro.getErrorMessage("tamanho_minimo", argumentos,
 					"validacaoId");
 		} else if (!memoriaVirtualEJB.disponibilidadeId(this.id)) {
 			String[] argumentos = { "id" };
-			MensagensErro.getErrorMessage("ja_cadastrado", argumentos,
+			MensagensDeErro.getErrorMessage("ja_cadastrado", argumentos,
 					"validacaoId");
 		}
 	}
@@ -101,18 +128,22 @@ public class CadastrarUsuarioMB {
 	}
 
 	public void validateEmail(AjaxBehaviorEvent event) {
+		this.validateEmail();
+	}
+
+	public void validateEmail() {
 
 		if (this.email.equals("")) {
 			String[] argumentos = { "email" };
-			MensagensErro.getErrorMessage("campo_vazio", argumentos,
+			MensagensDeErro.getErrorMessage("campo_vazio", argumentos,
 					"validacaoEmail");
-		} else if (!memoriaVirtualEJB.validarEmail(this.email)) {
+		} else if (!ValidacoesDeCampos.validarFormatoEmail(this.email)) {
 			String[] argumentos = { "email" };
-			MensagensErro.getErrorMessage("formato_invalido", argumentos,
+			MensagensDeErro.getErrorMessage("formato_invalido", argumentos,
 					"validacaoEmail");
 		} else if (!memoriaVirtualEJB.disponibilidadeEmail(this.email)) {
 			String[] argumentos = { "email" };
-			MensagensErro.getErrorMessage("ja_cadastrado", argumentos,
+			MensagensDeErro.getErrorMessage("ja_cadastrado", argumentos,
 					"validacaoEmail");
 		}
 	}
@@ -126,15 +157,19 @@ public class CadastrarUsuarioMB {
 	}
 
 	public void validateSenha(AjaxBehaviorEvent event) {
+		this.validateSenha();
+	}
+
+	public void validateSenha() {
 		if (this.senha.equals("")) {
 			String[] argumentos = { "senha" };
-			MensagensErro.getErrorMessage("campo_vazio", argumentos,
+			MensagensDeErro.getErrorMessage("campo_vazio", argumentos,
 					"validacaoSenha");
 		} else if (this.senha.length() < 6) {
 			String[] argumentos = { "senha", "senha_minima" };
-			MensagensErro.getErrorMessage("tamanho_minimo", argumentos,
+			MensagensDeErro.getErrorMessage("tamanho_minimo", argumentos,
 					"validacaoSenha");
-		} 
+		}
 	}
 
 	public String getNomeCompleto() {
@@ -146,9 +181,13 @@ public class CadastrarUsuarioMB {
 	}
 
 	public void validateNomeCompleto(AjaxBehaviorEvent event) {
+		this.validateNomeCompleto();
+	}
+
+	public void validateNomeCompleto() {
 		if (this.nomeCompleto.equals("")) {
 			String[] argumentos = { "nome_completo" };
-			MensagensErro.getErrorMessage("campo_vazio", argumentos,
+			MensagensDeErro.getErrorMessage("campo_vazio", argumentos,
 					"validacaoNomeCompleto");
 		}
 	}
@@ -162,26 +201,19 @@ public class CadastrarUsuarioMB {
 	}
 
 	public void validateTelefone(AjaxBehaviorEvent event) {
-		String formato = "\\([0-9]{2}?\\)[0-9]{4}?\\-[0-9]{4}?";
+		this.validateTelefone();
+	}
 
+	public void validateTelefone() {
 		if (this.telefone.equals("")) {
 			String[] argumentos = { "telefone" };
-			MensagensErro.getErrorMessage("campo_vazio", argumentos,
+			MensagensDeErro.getErrorMessage("campo_vazio", argumentos,
 					"validacaoTelefone");
-		} else if (!this.telefone.matches(formato)) {
+		} else if (!ValidacoesDeCampos.validarFormatoTelefone(this.telefone)) {
 			String[] argumentos = { "telefone" };
-			MensagensErro.getErrorMessage("formato_invalido", argumentos,
+			MensagensDeErro.getErrorMessage("formato_invalido", argumentos,
 					"validacaoTelefone");
 		}
-
-	}
-
-	public String getValidacao() {
-		return validacao;
-	}
-
-	public void setValidacao(String validacao) {
-		this.validacao = validacao;
 	}
 
 	public String getConfirmacaoSenha() {
@@ -193,15 +225,19 @@ public class CadastrarUsuarioMB {
 	}
 
 	public void validateConfirmacaoSenha(AjaxBehaviorEvent event) {
+		this.validateConfirmacaoSenha();
+	}
+
+	public void validateConfirmacaoSenha() {
 		if (confirmacaoSenha.equals("")) {
 			String[] argumentos = { "confirmacao_senha" };
-			MensagensErro.getErrorMessage("campo_vazio", argumentos,
-			"validacaoConfirmacaoSenha");
+			MensagensDeErro.getErrorMessage("campo_vazio", argumentos,
+					"validacaoConfirmacaoSenha");
 		} else if (!this.confirmacaoSenha.equals(this.senha)) {
 			String[] argumentos = { "confirmacao_senha", "senha" };
-			MensagensErro.getErrorMessage("confirmacao_errado", argumentos,
-			"validacaoConfirmacaoSenha");
-		} 
+			MensagensDeErro.getErrorMessage("confirmacao_errado", argumentos,
+					"validacaoConfirmacaoSenha");
+		}
 	}
 
 }
