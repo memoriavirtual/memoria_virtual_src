61123bc35d0e8e8f90f8715dc235e828f24afd15
baldacim <baldacim@699894a6-35f8-415d-b816-a1f94c44af83>
2011-10-19 20:59:47 +0000

    git-svn-id: file:///home/brauner/Desktop/DEV_DUMP_19.05.2015/svn_storage/memoria_virtual_src@373 699894a6-35f8-415d-b816-a1f94c44af83

---
+++ trunk/memoriavirtualWeb/src/br/usp/memoriavirtual/utils/ValidacoesDeCampos.java
@@ -0,0 +1,35 @@
+package br.usp.memoriavirtual.utils;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class ValidacoesDeCampos {
+
+	public static boolean validarFormatoEmail(String email) {
+
+		String regexp = "[a-z0-9!#$%&’*+/=?^_‘{|}~-]+(?:\\."
+				+ "[a-z0-9!#$%&’*+/=?^_‘{|}~-]+)*@"
+				+ "(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?";
+		Pattern pattern = Pattern.compile(regexp);
+		Matcher matcher = pattern.matcher(email);
+
+		if (!matcher.matches())
+			return false;
+
+		return true;
+	}
+	
+	public static boolean validarFormatoTelefone(String telefone) {
+
+		String regexp = "\\([0-9]{2}?\\)[0-9]{4}?\\-[0-9]{4}?";
+		Pattern pattern = Pattern.compile(regexp);
+		Matcher matcher = pattern.matcher(telefone);
+
+		if (!matcher.matches())
+			return false;
+
+		return true;
+	}
+	
+	
+}
