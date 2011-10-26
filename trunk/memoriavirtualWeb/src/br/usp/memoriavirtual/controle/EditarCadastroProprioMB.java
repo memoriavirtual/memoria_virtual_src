35326b0dbb65b7deaf9760596afa8a9b664350b8
diegominatel <diegominatel@699894a6-35f8-415d-b816-a1f94c44af83>
2011-10-26 16:43:09 +0000

    git-svn-id: file:///home/brauner/Desktop/DEV_DUMP_19.05.2015/svn_storage/memoria_virtual_src@395 699894a6-35f8-415d-b816-a1f94c44af83

--- trunk/memoriavirtualWeb/src/br/usp/memoriavirtual/controle/EditarCadastroProprioMB.java
+++ trunk/memoriavirtualWeb/src/br/usp/memoriavirtual/controle/EditarCadastroProprioMB.java
@@ -6,6 +6,7 @@ import javax.servlet.http.HttpServletRequest;
 
 import br.usp.memoriavirtual.modelo.entidades.Usuario;
 import br.usp.memoriavirtual.modelo.fachadas.remoto.EditarCadastroProprioRemote;
+import br.usp.memoriavirtual.utils.MensagensDeErro;
 
 public class EditarCadastroProprioMB {
 
@@ -14,24 +15,28 @@ public class EditarCadastroProprioMB {
 	private Usuario usuario;
 	private String novoEmail;
 	private String novoNomeCompleto;
+	private String novoTelefone;
 	private String novaSenha;
+	private String confirmacaoNovaSenha;
 
 	public EditarCadastroProprioMB() {
 		HttpServletRequest request = (HttpServletRequest) FacesContext
 				.getCurrentInstance().getExternalContext().getRequest();
 		this.usuario = (Usuario) request.getSession().getAttribute("usuario");
+		setNovoNomeCompleto(getNomeCompleto());
+		setNovoEmail(getEmail());
+		setNovoTelefone(getTelefone());
 	}
 
 	public String editarCadastroProprio() {
 		if (this.novoEmail == null || this.novoNomeCompleto == null
 				|| this.novaSenha == null)
 			return "Incompleto";
-		usuario = this.editarCadastroProprioEJB
-				.recuperarDadosUsuario("mvirtual");
 		try {
 			this.editarCadastroProprioEJB.atualizarDadosUsuario(
-					usuario.getId(), this.novoEmail, this.novoNomeCompleto,
-					this.novaSenha);
+					usuario.getId(), getNovoEmail(), getNovoNomeCompleto(),
+					getNovoTelefone(), getNovaSenha());
+			MensagensDeErro.getSucessMessage("cadastro_concluido", "resultado");
 			return "Sucesso";
 			/** Atualizar os dados da sessão de usuario */
 		} catch (Exception e) {
@@ -47,6 +52,10 @@ public class EditarCadastroProprioMB {
 		return this.usuario.getEmail();
 	}
 
+	public String getTelefone() {
+		return this.usuario.getTelefone();
+	}
+
 	public String getSenha() {
 		return this.usuario.getSenha();
 	}
@@ -59,17 +68,24 @@ public class EditarCadastroProprioMB {
 		this.novoNomeCompleto = novoNomeCompleto;
 	}
 
-	public boolean setNovaSenha(String novaSenha, String confirmaNovaSenha) {
-		if (novaSenha == confirmaNovaSenha) {
-			this.novaSenha = novaSenha;
-			return true;
-		} else
-			return false;
+	public void setNovoTelefone(String novoTelefone) {
+		this.novoTelefone = novoTelefone;
+	}
 
+	public void setNovaSenha(String novaSenha) {
+		this.novaSenha = novaSenha;
+	}
+
+	public void setConfirmacaoNovaSenha(String confirmacaoNovaSenha) {
+		this.confirmacaoNovaSenha = confirmacaoNovaSenha;
 	}
 
 	public String getNovoNomeCompleto() {
 		return this.novoNomeCompleto;
+	}
+
+	public String getNovoTelefone() {
+		return this.novoTelefone;
 	}
 
 	public String getNovoEmail() {
@@ -78,6 +94,34 @@ public class EditarCadastroProprioMB {
 
 	public String getNovaSenha() {
 		return this.novaSenha;
+	}
+
+	public String getConfirmacaoNovaSenha() {
+		return this.confirmacaoNovaSenha;
+	}
+
+	public void validateSenha() {
+		if (this.novaSenha.equals("")) {
+			String[] argumentos = { "senha" };
+			MensagensDeErro.getErrorMessage("campo_vazio", argumentos,
+					"validacaoSenha");
+		} else if (this.novaSenha.length() < 6) {
+			String[] argumentos = { "senha", "senha_minima" };
+			MensagensDeErro.getErrorMessage("tamanho_minimo", argumentos,
+					"validacaoSenha");
+		}
+	}
+
+	public void validateConfirmacaoSenha() {
+		if (confirmacaoNovaSenha.equals("")) {
+			String[] argumentos = { "confirmacao_senha" };
+			MensagensDeErro.getErrorMessage("campo_vazio", argumentos,
+					"validacaoConfirmacaoSenha");
+		} else if (!this.confirmacaoNovaSenha.equals(this.novaSenha)) {
+			String[] argumentos = { "confirmacao_senha", "senha" };
+			MensagensDeErro.getErrorMessage("confirmacao_errado", argumentos,
+					"validacaoConfirmacaoSenha");
+		}
 	}
 
 }
