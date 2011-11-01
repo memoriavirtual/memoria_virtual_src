f2b58b6b5a74d4308346d4e05b6c982378ac09b4
gmarques <gmarques@699894a6-35f8-415d-b816-a1f94c44af83>
2011-11-01 08:07:57 +0000

    Implementação do CDU obter nova senha.
    git-svn-id: file:///home/brauner/Desktop/DEV_DUMP_19.05.2015/svn_storage/memoria_virtual_src@409 699894a6-35f8-415d-b816-a1f94c44af83

--- trunk/memoriavirtualEJB/ejbModule/br/usp/memoriavirtual/modelo/fachadas/ObterNovaSenha.java
+++ trunk/memoriavirtualEJB/ejbModule/br/usp/memoriavirtual/modelo/fachadas/ObterNovaSenha.java
@@ -7,6 +7,7 @@ import javax.ejb.EJB;
 import javax.ejb.Stateless;
 import javax.mail.MessagingException;
 import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
 import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
 
@@ -31,44 +32,43 @@ public class ObterNovaSenha implements ObterNovaSenhaRemote {
 
 	@Override
 	public void obterNovaSenha(String email) throws ModeloException {
-		
+
 		Usuario usuario = null;
-		String token = null;
 
 		usuario = recuperarUsuario(email);
 		if (usuario != null) {
-			token = Usuario.gerarHash(email
-					+ Integer.toString(new Random().nextInt()));
 
-			usuario.setSenhaTemporaria(token);
+			usuario.setSenha(email + new Random().nextInt());
 			this.entityManager.flush();
-			
+
 			try {
 				String assunto = "Recuperação de senha do Memoria Virtual";
 				String mensagem = "Você foi solicitou uma nova senha do Memoria Virtual.  "
 						+ "Para cadastrar uma nova senha, entre no link a seguir: "
-						+ this.memoriaVirtual.getEnderecoServidor()
-								.getCanonicalHostName()
-						+ "/ObterNovaSenha.jsf?Validacao="
-						+ token
-						+ "&email="
-						+ email;
+						+ this.memoriaVirtual.getEnderecoServidor().getCanonicalHostName()							
+						+ "/cadastrarnovasenha.jsf?email="
+						+ email
+						+ "&validacao="
+						+ usuario.getSenha();
 				this.memoriaVirtual.enviarEmail(email, assunto, mensagem);
 			} catch (IOException e) {
 				e.printStackTrace();
-				throw new ModeloException("Erro no servidor");
+				throw new ModeloException("Erro no servidor!");
 			} catch (MessagingException e) {
 				e.printStackTrace();
-				throw new ModeloException("Erro ao enviar o email");
+				throw new ModeloException("Erro ao enviar o email!");
 			}
+			
+			throw new ModeloException("Email enviado com sucesso!");
 
 		} else
-			throw new ModeloException("Email não encontrado");
+			throw new ModeloException("Email não encontrado!");
 	}
-	
-	public void cadastrarNovaSenha(String email, String token, String novaSenha) throws ModeloException{
+
+	public void cadastrarNovaSenha(String email, String token, String novaSenha)
+			throws ModeloException {
 		Usuario usuario = null;
-		
+
 		usuario = recuperarUsuario(email, token);
 		if (usuario != null) {
 			usuario.setSenha(novaSenha);
@@ -77,25 +77,48 @@ public class ObterNovaSenha implements ObterNovaSenhaRemote {
 			throw new ModeloException("Erro ao processar sua solicitação.");
 	}
 
-	private Usuario recuperarUsuario(String email) {
+	/**
+	 * 
+	 * @param email
+	 * @return Usuario com o email fornecido
+	 * @throws ModeloException 
+	 */
+	private Usuario recuperarUsuario(String email) throws ModeloException {
 		Usuario usuario = null;
 
 		Query query = this.entityManager
 				.createQuery("SELECT u FROM Usuario u WHERE u.email = :email");
 		query.setParameter("email", email);
-		usuario = (Usuario) query.getSingleResult();
-
+		try {
+			usuario = (Usuario) query.getSingleResult();
+		} catch (NoResultException e) {
+			throw new ModeloException("Email não cadastrado!");
+		}
 		return usuario;
 	}
-	
-	private Usuario recuperarUsuario(String email, String senha) {
+
+	/**
+	 * 
+	 * @param email
+	 * @param senha
+	 * @return Usuario com o email e senha fornecidos (A senha sera comparada
+	 *         com a senha armazenada no banco, sem calcular hash, pois é apenas
+	 *         um token para a recuperçao da senha)
+	 * @throws ModeloException 
+	 * 
+	 */
+	private Usuario recuperarUsuario(String email, String senha) throws ModeloException {
 		Usuario usuario = null;
 
 		Query query = this.entityManager
 				.createQuery("SELECT u FROM Usuario u WHERE u.email = :email AND u.senha = :senha");
 		query.setParameter("email", email);
 		query.setParameter("senha", senha);
-		usuario = (Usuario) query.getSingleResult();
+		try {
+			usuario = (Usuario) query.getSingleResult();
+		} catch (NoResultException e) {
+			throw new ModeloException("Erro processar sua solicitação!");
+		}
 
 		return usuario;
 	}
