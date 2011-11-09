9686b35fdfdfa4b1d6d9f13a77219b76cc32ca79
baldacim <baldacim@699894a6-35f8-415d-b816-a1f94c44af83>
2011-11-09 16:20:06 +0000

    git-svn-id: file:///home/brauner/Desktop/DEV_DUMP_19.05.2015/svn_storage/memoria_virtual_src@425 699894a6-35f8-415d-b816-a1f94c44af83

--- trunk/memoriavirtualEJB/ejbModule/br/usp/memoriavirtual/modelo/fachadas/remoto/EnviarConviteRemote.java
+++ trunk/memoriavirtualEJB/ejbModule/br/usp/memoriavirtual/modelo/fachadas/remoto/EnviarConviteRemote.java
@@ -1,5 +1,6 @@
 package br.usp.memoriavirtual.modelo.fachadas.remoto;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.ejb.Remote;
@@ -7,11 +8,12 @@ import javax.ejb.Remote;
 import br.usp.memoriavirtual.modelo.entidades.Grupo;
 import br.usp.memoriavirtual.modelo.entidades.Instituicao;
 import br.usp.memoriavirtual.modelo.entidades.Usuario;
+import br.usp.memoriavirtual.modelo.fachadas.ModeloException;
 
 @Remote
 public interface EnviarConviteRemote {
 
-    public String enviarConvite(String emails, String mensagem, String validade, String instituicao, String nivelAcesso);
+    public void enviarConvite(ArrayList<String> emails, String mensagem, String validade, String instituicao, String nivelAcesso) throws ModeloException;
     public List<Instituicao> getInstituicoes(Grupo grupo, Usuario usuario);
     public List<Instituicao> getInstituicoes();
     public List<Grupo> getGrupos();
