76d886fece687f214ded85961ac6e9fab807dffc
diegogomes <diegogomes@699894a6-35f8-415d-b816-a1f94c44af83>
2011-11-23 18:58:03 +0000

    git-svn-id: file:///home/brauner/Desktop/DEV_DUMP_19.05.2015/svn_storage/memoria_virtual_src@457 699894a6-35f8-415d-b816-a1f94c44af83

--- trunk/memoriavirtualEJB/ejbModule/br/usp/memoriavirtual/modelo/fachadas/MemoriaVirtual.java
+++ trunk/memoriavirtualEJB/ejbModule/br/usp/memoriavirtual/modelo/fachadas/MemoriaVirtual.java
@@ -3,8 +3,10 @@ package br.usp.memoriavirtual.modelo.fachadas;
 import java.io.IOException;
 import java.net.InetAddress;
 import java.net.NetworkInterface;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.Enumeration;
+import java.util.List;
 
 import javax.annotation.Resource;
 import javax.ejb.Singleton;
@@ -17,7 +19,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.NoResultException;
 import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
-
+import br.usp.memoriavirtual.modelo.entidades.Instituicao;
 import br.usp.memoriavirtual.modelo.fachadas.remoto.MemoriaVirtualRemote;
 
 /**
@@ -48,15 +50,20 @@ public class MemoriaVirtual implements MemoriaVirtualRemote {
 	public InetAddress getEnderecoServidor() throws IOException {
 
 		if (enderecoServidor == null) {
-			Enumeration<NetworkInterface> interfaces = NetworkInterface.getNetworkInterfaces();
+			Enumeration<NetworkInterface> interfaces = NetworkInterface
+					.getNetworkInterfaces();
 
 			while (interfaces.hasMoreElements()) {
-				NetworkInterface interfaceRede = (NetworkInterface) interfaces.nextElement();
+				NetworkInterface interfaceRede = (NetworkInterface) interfaces
+						.nextElement();
 				if (interfaceRede.isUp()) {
-					Enumeration<InetAddress> enderecosRede = interfaceRede.getInetAddresses();
+					Enumeration<InetAddress> enderecosRede = interfaceRede
+							.getInetAddresses();
 					while (enderecosRede.hasMoreElements()) {
-						InetAddress enderecoRede = (InetAddress) enderecosRede.nextElement();
-						if (enderecoRede.isReachable(10) && !enderecoRede.isLoopbackAddress()
+						InetAddress enderecoRede = (InetAddress) enderecosRede
+								.nextElement();
+						if (enderecoRede.isReachable(10)
+								&& !enderecoRede.isLoopbackAddress()
 								&& !enderecoRede.isAnyLocalAddress()) {
 							enderecoServidor = enderecoRede;
 							return enderecoServidor;
@@ -70,7 +77,8 @@ public class MemoriaVirtual implements MemoriaVirtualRemote {
 
 	public boolean disponibilidadeId(String id) {
 
-		Query query = entityManager.createQuery("SELECT u FROM Usuario u WHERE u.id = :id");
+		Query query = entityManager
+				.createQuery("SELECT u FROM Usuario u WHERE u.id = :id");
 		query.setParameter("id", id);
 
 		try {
@@ -85,7 +93,8 @@ public class MemoriaVirtual implements MemoriaVirtualRemote {
 
 	public boolean disponibilidadeNomeInstituicao(String Nome) {
 
-		Query query = entityManager.createQuery("SELECT u FROM Instituicao u WHERE u.nome = :nome");
+		Query query = entityManager
+				.createQuery("SELECT u FROM Instituicao u WHERE u.nome = :nome");
 		query.setParameter("nome", Nome);
 
 		try {
@@ -100,7 +109,8 @@ public class MemoriaVirtual implements MemoriaVirtualRemote {
 
 	public boolean disponibilidadeEmail(String email) {
 
-		Query query = entityManager.createQuery("SELECT u FROM Usuario u WHERE u.email = :email");
+		Query query = entityManager
+				.createQuery("SELECT u FROM Usuario u WHERE u.email = :email");
 		query.setParameter("email", email);
 
 		try {
@@ -112,10 +122,12 @@ public class MemoriaVirtual implements MemoriaVirtualRemote {
 		return true;
 	}
 
-	public void enviarEmail(String destinatario, String assunto, String mensagem) throws MessagingException {
+	public void enviarEmail(String destinatario, String assunto, String mensagem)
+			throws MessagingException {
 		Message message = new MimeMessage(this.mailSession);
 		message.setFrom();
-		message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(destinatario, false));
+		message.setRecipients(Message.RecipientType.TO,
+				InternetAddress.parse(destinatario, false));
 		message.setSubject(assunto);
 		Date timeStamp = new Date();
 		message.setText(mensagem);
@@ -133,7 +145,8 @@ public class MemoriaVirtual implements MemoriaVirtualRemote {
 		if (msgOriginal.length() % 2 != 0) {
 			for (int i = 0; i < msgOriginal.length(); i++) {
 				if ((i + 1) % 2 != 0) {
-					msgNova = msgNova.concat("" + msgOriginal.charAt(msgOriginal.length() - 1 - i));
+					msgNova = msgNova.concat(""
+							+ msgOriginal.charAt(msgOriginal.length() - 1 - i));
 				} else {
 					msgNova = msgNova.concat("" + msgOriginal.charAt(i));
 				}
@@ -143,18 +156,22 @@ public class MemoriaVirtual implements MemoriaVirtualRemote {
 			for (int i = 0; i < msgOriginal.length(); i++) {
 				if (i + 1 != msgOriginal.length() - 1 - i) {
 					if ((i + 1 - aux) % 2 != 0) {
-						msgNova = msgNova.concat("" + msgOriginal.charAt(msgOriginal.length() - 1 - i));
+						msgNova = msgNova.concat(""
+								+ msgOriginal.charAt(msgOriginal.length() - 1
+										- i));
 					} else {
 						msgNova = msgNova.concat("" + msgOriginal.charAt(i));
 					}
 				} else {
 					if (msgOriginal.length() % 4 == 0) {
 						msgNova = msgNova.concat("" + msgOriginal.charAt(i));
-						msgNova = msgNova.concat("" + msgOriginal.charAt(i + 1));
+						msgNova = msgNova
+								.concat("" + msgOriginal.charAt(i + 1));
 						aux++;
 						i++;
 					} else {
-						msgNova = msgNova.concat("" + msgOriginal.charAt(i + 1));
+						msgNova = msgNova
+								.concat("" + msgOriginal.charAt(i + 1));
 						msgNova = msgNova.concat("" + msgOriginal.charAt(i));
 						aux++;
 						i++;
@@ -163,6 +180,32 @@ public class MemoriaVirtual implements MemoriaVirtualRemote {
 			}
 		}
 		return msgNova;
+	}
+
+	
+
+	/**
+	 * Método de sugestões para instituição
+	 * 
+	 * @param pnome String para a qual o sistema gera sugestões
+	 * @return Lista de Instituições que começam com a String de parâmetro
+	 */
+	@SuppressWarnings("unchecked")
+	public List<Instituicao> listarInstuicoes(String pnome) {
+		List<Instituicao> ins = new ArrayList<Instituicao>();
+		Query query;
+
+		query = entityManager
+				.createQuery("SELECT a FROM Instituicao a WHERE a.nome LIKE padrao ORDER BY a.nome ");
+
+		query.setParameter("padrao", pnome + "%");
+		try {
+			ins = (List<Instituicao>) query.getResultList();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return ins;
 	}
 
 }
