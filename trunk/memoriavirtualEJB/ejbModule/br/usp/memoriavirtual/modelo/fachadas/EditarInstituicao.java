bc3509e649cc73722c0d38a2e3eee9eaf243103f
joesantos <joesantos@699894a6-35f8-415d-b816-a1f94c44af83>
2011-12-08 02:22:54 +0000

    cdu 39
    git-svn-id: file:///home/brauner/Desktop/DEV_DUMP_19.05.2015/svn_storage/memoria_virtual_src@470 699894a6-35f8-415d-b816-a1f94c44af83

--- trunk/memoriavirtualEJB/ejbModule/br/usp/memoriavirtual/modelo/fachadas/EditarInstituicao.java
+++ trunk/memoriavirtualEJB/ejbModule/br/usp/memoriavirtual/modelo/fachadas/EditarInstituicao.java
@@ -2,10 +2,12 @@ package br.usp.memoriavirtual.modelo.fachadas;
 
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
-import javax.persistence.NoResultException;
 import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
+
+import br.usp.memoriavirtual.modelo.entidades.Grupo;
 import br.usp.memoriavirtual.modelo.entidades.Instituicao;
+import br.usp.memoriavirtual.modelo.entidades.Usuario;
 import br.usp.memoriavirtual.modelo.fachadas.remoto.EditarInstituicaoRemote;
 import br.usp.memoriavirtual.modelo.fachadas.remoto.MemoriaVirtualRemote;
 
@@ -22,21 +24,74 @@ public class EditarInstituicao implements EditarInstituicaoRemote {
 	public EditarInstituicao() {
 	}
 
+	/**
+	 * Metodo auxiliar para recuperar instituicao
+	 * 
+	 * @param pnome
+	 *            String - Nome da instituicao
+	 * @return Instituicao correspondente ao nome
+	 * @throws ModeloException
+	 *             Em caso de erro
+	 */
+	public Instituicao getInstituicao(String pnome) throws ModeloException {
+		Instituicao instituicao;
+		Query query;
+		query = this.entityManager
+				.createQuery("SELECT a FROM Instituicao a WHERE a.nome = :nome");
+		query.setParameter("nome", pnome);
+		try {
+			instituicao = (Instituicao) query.getSingleResult();
+		} catch (Exception e) {
+			throw new ModeloException(e);
+		}
+		return instituicao;
+	}
+
+	/**
+	 * Metodo auxiliar para recuperar instituicao
+	 * 
+	 * @param pnome
+	 *            String - Nome da instituicao
+	 * @param grupo
+	 *            Grupo ao qual o usuario pertence
+	 * @param usuario
+	 *            Usuario que faz a requisicao
+	 * @return Instituicao correspondente ao nome
+	 * @throws ModeloException
+	 *             Em caso de erro
+	 */
+	public Instituicao getInstituicao(String pnome, Grupo grupo, Usuario usuario)
+			throws ModeloException {
+		Instituicao instituicao;
+		Query query;
+		query = this.entityManager
+				.createQuery("SELECT a FROM Acesso a WHERE a.nome =: nome AND a.grupo =: grupo AND a.usuario =: usuario");
+		query.setParameter("nome", pnome);
+		query.setParameter("grupo", grupo);
+		query.setParameter("usuario", usuario);
+		try {
+			instituicao = (Instituicao) query.getSingleResult();
+		} catch (Exception e) {
+			throw new ModeloException(e);
+		}
+		return instituicao;
+	}
+
 	@Override
 	public void editarInstituicao(Instituicao instituicao, String nome,
 			String localizacao, String endereco, String cidade, String estado,
 			String cep, String telefone) throws ModeloException {
-		
+
 		Instituicao managedInstituicao;
-		
-		try{
-		//Verifica a existencia da instituicao a ser alterada
-		managedInstituicao = this.entityManager.find(Instituicao.class, instituicao);
-		}
-		catch(Exception e){
+
+		try {
+			// Verifica a existencia da instituicao a ser alterada
+			managedInstituicao = this.entityManager.find(Instituicao.class,
+					instituicao);
+		} catch (Exception e) {
 			throw new ModeloException(e);
 		}
-		if(managedInstituicao == null){
+		if (managedInstituicao == null) {
 			throw new ModeloException("Instituicao Inexistente");
 		}
 
--- trunk/memoriavirtualEJB/ejbModule/br/usp/memoriavirtual/modelo/fachadas/remoto/EditarInstituicaoRemote.java
+++ trunk/memoriavirtualEJB/ejbModule/br/usp/memoriavirtual/modelo/fachadas/remoto/EditarInstituicaoRemote.java
@@ -2,7 +2,9 @@ package br.usp.memoriavirtual.modelo.fachadas.remoto;
 
 import javax.ejb.Remote;
 
+import br.usp.memoriavirtual.modelo.entidades.Grupo;
 import br.usp.memoriavirtual.modelo.entidades.Instituicao;
+import br.usp.memoriavirtual.modelo.entidades.Usuario;
 import br.usp.memoriavirtual.modelo.fachadas.ModeloException;
 
 /**
@@ -33,8 +35,14 @@ import br.usp.memoriavirtual.modelo.fachadas.ModeloException;
 @Remote
 public interface EditarInstituicaoRemote {
 
-	public void editarInstituicao(Instituicao instituicao, String nome, String novoLocalizacao,
-			String novoEndereco, String novoCidade, String novoEstado,
-			String novoCep, String novoTelefone) throws ModeloException;
+	public void editarInstituicao(Instituicao instituicao, String nome,
+			String novoLocalizacao, String novoEndereco, String novoCidade,
+			String novoEstado, String novoCep, String novoTelefone)
+			throws ModeloException;
+
+	public Instituicao getInstituicao(String pnome) throws ModeloException;
+
+	public Instituicao getInstituicao(String pnome, Grupo grupo, Usuario usuario)
+			throws ModeloException;
 
 }
