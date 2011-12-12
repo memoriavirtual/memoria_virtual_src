b2018f6d0a35da2f954e40062b8cdf9a1377049e
diegogomes <diegogomes@699894a6-35f8-415d-b816-a1f94c44af83>
2011-12-12 02:11:35 +0000

    git-svn-id: file:///home/brauner/Desktop/DEV_DUMP_19.05.2015/svn_storage/memoria_virtual_src@476 699894a6-35f8-415d-b816-a1f94c44af83

--- trunk/memoriavirtualWeb/src/br/usp/memoriavirtual/controle/ExcluirInstituicaoMB.java
+++ trunk/memoriavirtualWeb/src/br/usp/memoriavirtual/controle/ExcluirInstituicaoMB.java
@@ -1,7 +1,6 @@
 package br.usp.memoriavirtual.controle;
 
 import java.io.Serializable;
-import java.util.ArrayList;
 import java.util.List;
 import javax.ejb.EJB;
 import javax.faces.application.FacesMessage;
@@ -9,13 +8,7 @@ import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 import javax.faces.context.FacesContext;
 import javax.faces.event.AjaxBehaviorEvent;
-import javax.faces.model.SelectItem;
-
-import br.usp.memoriavirtual.utils.ValidacoesDeCampos;
-import br.usp.memoriavirtual.utils.MensagensDeErro;
-import br.usp.memoriavirtual.modelo.entidades.Grupo;
 import br.usp.memoriavirtual.modelo.entidades.Instituicao;
-import br.usp.memoriavirtual.modelo.entidades.Usuario;
 import br.usp.memoriavirtual.modelo.fachadas.ModeloException;
 import br.usp.memoriavirtual.modelo.fachadas.remoto.EditarInstituicaoRemote;
 import br.usp.memoriavirtual.modelo.fachadas.remoto.MemoriaVirtualRemote;
@@ -28,6 +21,10 @@ public class ExcluirInstituicaoMB implements Serializable {
 	//private ExcluirInstituicaoRemote excluirInstituicaoEJB;
 		
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	@EJB
 	private MemoriaVirtualRemote memoriaVirtualEJB;
 	private EditarInstituicaoRemote editarInstituicaoEJB;
