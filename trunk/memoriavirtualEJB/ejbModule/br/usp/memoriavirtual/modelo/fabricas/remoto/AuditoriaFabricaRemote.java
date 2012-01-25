56e6f340278a211747cdf4f3d42352bd306dda2a
diegogomes <diegogomes@699894a6-35f8-415d-b816-a1f94c44af83>
2012-01-25 18:55:43 +0000

    git-svn-id: file:///home/brauner/Desktop/DEV_DUMP_19.05.2015/svn_storage/memoria_virtual_src@533 699894a6-35f8-415d-b816-a1f94c44af83

--- trunk/memoriavirtualEJB/ejbModule/br/usp/memoriavirtual/modelo/fabricas/remoto/AuditoriaFabricaRemote.java
+++ trunk/memoriavirtualEJB/ejbModule/br/usp/memoriavirtual/modelo/fabricas/remoto/AuditoriaFabricaRemote.java
@@ -2,7 +2,14 @@ package br.usp.memoriavirtual.modelo.fabricas.remoto;
 
 import javax.ejb.Remote;
 
+import br.usp.memoriavirtual.modelo.entidades.Usuario;
+
 @Remote
 public interface AuditoriaFabricaRemote {
-
+	/**
+	 * 
+	 * @param autorAcao
+	 * @param atributoSignificativo
+	 */
+	public void auditarExclusaoInstituicao(Usuario autorAcao,String atributoSignificativo);
 }
