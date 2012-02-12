4482589da7c0eeec2630cbce0024b4c983ebfddb
diegogomes <diegogomes@699894a6-35f8-415d-b816-a1f94c44af83>
2012-02-12 07:13:31 +0000

    git-svn-id: file:///home/brauner/Desktop/DEV_DUMP_19.05.2015/svn_storage/memoria_virtual_src@588 699894a6-35f8-415d-b816-a1f94c44af83

--- trunk/memoriavirtualEJB/ejbModule/br/usp/memoriavirtual/modelo/fachadas/ExcluirInstituicao.java
+++ trunk/memoriavirtualEJB/ejbModule/br/usp/memoriavirtual/modelo/fachadas/ExcluirInstituicao.java
@@ -171,7 +171,7 @@ public class ExcluirInstituicao implements ExcluirInstituicaoRemote {
 				throw new ModeloException(e);
 			}
 		}else{//Caso Acao falso a instituicao volta a funcionar normalmente
-			this.marcarInstituicaoExcluida(instituicao, acao);
+			this.marcarInstituicaoExcluida(instituicao,! acao);
 		}
 	}
 	public void enviaremail(String Email,String assunto,String textoEmail){
@@ -220,7 +220,6 @@ public class ExcluirInstituicao implements ExcluirInstituicaoRemote {
 
 	public void marcarInstituicaoExcluida(Instituicao instituicao,boolean marca)
 			throws ModeloException{
-
 		Query query;
 		query = this.entityManager
 				.createQuery("UPDATE Instituicao a SET a.validade = :validade WHERE  a.id = :id");
@@ -237,6 +236,7 @@ public class ExcluirInstituicao implements ExcluirInstituicaoRemote {
 	public void registrarAprovacao(Usuario validador, Instituicao instituicao,
 			Date dataValidade){
 		Date data = new Date();
+		instituicao = this.entityManager.find(Instituicao.class, instituicao.getId());
 		Usuario u = entityManager.find(Usuario.class, validador.getId());
 		Aprovacao aprovacao = new Aprovacao( data , u , dataValidade , instituicao.getNome() , "Instituicao");
 		this.entityManager.persist(aprovacao);
