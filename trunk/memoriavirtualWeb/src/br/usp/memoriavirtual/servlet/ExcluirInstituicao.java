061561575fb2ad6906641bcf3394a4c869a38d33
diegogomes <diegogomes@699894a6-35f8-415d-b816-a1f94c44af83>
2012-02-21 17:16:23 +0000

    git-svn-id: file:///home/brauner/Desktop/DEV_DUMP_19.05.2015/svn_storage/memoria_virtual_src@610 699894a6-35f8-415d-b816-a1f94c44af83

--- trunk/memoriavirtualWeb/src/br/usp/memoriavirtual/servlet/ExcluirInstituicao.java
+++ trunk/memoriavirtualWeb/src/br/usp/memoriavirtual/servlet/ExcluirInstituicao.java
@@ -15,7 +15,6 @@ import br.usp.memoriavirtual.modelo.entidades.Aprovacao;
 import br.usp.memoriavirtual.modelo.entidades.EnumTipoAcao;
 import br.usp.memoriavirtual.modelo.entidades.Instituicao;
 import br.usp.memoriavirtual.modelo.entidades.ItemAuditoria;
-import br.usp.memoriavirtual.modelo.entidades.Usuario;
 import br.usp.memoriavirtual.modelo.fachadas.ModeloException;
 import br.usp.memoriavirtual.modelo.fachadas.remoto.ExcluirInstituicaoRemote;
 import br.usp.memoriavirtual.utils.FacesUtil;
@@ -69,7 +68,7 @@ public class ExcluirInstituicao extends HttpServlet {
 		bean.setAprovacao(aprovacao);
 		bean.setRequisitor(itemAuditoria.getAutorAcao());
 		bean.setJustificativa(itemAuditoria.getNotas());
-		bean.Gerentes(false);	
+		bean.listarGerentes(false);	
 		response.sendRedirect("restrito/validarExclusao.jsf");
 		
 	}
