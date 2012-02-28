aa6fb9a66acf65e46a470bf45de4f71be71af1db
diegominatel <diegominatel@699894a6-35f8-415d-b816-a1f94c44af83>
2012-02-28 17:47:57 +0000

    git-svn-id: file:///home/brauner/Desktop/DEV_DUMP_19.05.2015/svn_storage/memoria_virtual_src@629 699894a6-35f8-415d-b816-a1f94c44af83

---
+++ trunk/memoriavirtualEJB/ejbModule/br/usp/memoriavirtual/modelo/fachadas/ExcluirUsuario.java
@@ -0,0 +1,7 @@
+package br.usp.memoriavirtual.modelo.fachadas;
+
+import br.usp.memoriavirtual.modelo.fachadas.remoto.ExcluirUsuarioRemote;
+
+public class ExcluirUsuario implements ExcluirUsuarioRemote {
+
+}
