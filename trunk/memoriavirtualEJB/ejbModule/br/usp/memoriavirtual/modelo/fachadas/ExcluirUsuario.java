75924f6f80f66c5e85648a174d12ef9be66098de
diegominatel <diegominatel@699894a6-35f8-415d-b816-a1f94c44af83>
2012-02-28 18:26:58 +0000

    git-svn-id: file:///home/brauner/Desktop/DEV_DUMP_19.05.2015/svn_storage/memoria_virtual_src@631 699894a6-35f8-415d-b816-a1f94c44af83

--- trunk/memoriavirtualEJB/ejbModule/br/usp/memoriavirtual/modelo/fachadas/ExcluirUsuario.java
+++ trunk/memoriavirtualEJB/ejbModule/br/usp/memoriavirtual/modelo/fachadas/ExcluirUsuario.java
@@ -1,7 +1,36 @@
 package br.usp.memoriavirtual.modelo.fachadas;
 
+import java.util.List;
+
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+
+import br.usp.memoriavirtual.modelo.entidades.Usuario;
 import br.usp.memoriavirtual.modelo.fachadas.remoto.ExcluirUsuarioRemote;
 
-public class ExcluirUsuario implements ExcluirUsuarioRemote {
 
+@Stateless (mappedName = "ExcluirUsuario")
+public class ExcluirUsuario implements ExcluirUsuarioRemote {
+	
+	@PersistenceContext(unitName = "memoriavirtual")
+	private EntityManager entityManager;
+
+	@SuppressWarnings("unchecked")
+	public List<Usuario> listarUsuarios(String parteNome) throws ModeloException {
+
+		List<Usuario> usuarios;
+		Query query;
+		query = this.entityManager
+				.createQuery("SELECT a FROM Usuario a WHERE a.administrador = FALSE AND a.nomeCompleto LIKE :parteNome% ORDER BY a.id ");
+		query.setParameter("parteNome", parteNome);
+		try {
+			usuarios = (List<Usuario>) query.getResultList();
+			return usuarios;
+		} catch (Exception e) {
+			throw new ModeloException(e);
+		}
+
+	}
 }
