fcb3cfe4a097917bc20716cffcc12f5849a80a58
diegominatel <diegominatel@699894a6-35f8-415d-b816-a1f94c44af83>
2012-02-28 19:32:57 +0000

    git-svn-id: file:///home/brauner/Desktop/DEV_DUMP_19.05.2015/svn_storage/memoria_virtual_src@634 699894a6-35f8-415d-b816-a1f94c44af83

--- trunk/memoriavirtualEJB/ejbModule/br/usp/memoriavirtual/modelo/fachadas/remoto/ExcluirUsuarioRemote.java
+++ trunk/memoriavirtualEJB/ejbModule/br/usp/memoriavirtual/modelo/fachadas/remoto/ExcluirUsuarioRemote.java
@@ -5,11 +5,10 @@ import java.util.List;
 import javax.ejb.Remote;
 
 import br.usp.memoriavirtual.modelo.entidades.Usuario;
-import br.usp.memoriavirtual.modelo.fachadas.ModeloException;
 
 @Remote
 public interface ExcluirUsuarioRemote {
-	
-	public List<Usuario> listarUsuarios(String parteNome) throws ModeloException;
+
+	public List<Usuario> listarUsuarios(String parteNome);
 
 }
