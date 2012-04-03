819291fb7642414b30678f2e2bc74f900888b1c0
diegogomes <diegogomes@699894a6-35f8-415d-b816-a1f94c44af83>
2012-04-03 21:04:06 +0000

    git-svn-id: file:///home/brauner/Desktop/DEV_DUMP_19.05.2015/svn_storage/memoria_virtual_src@673 699894a6-35f8-415d-b816-a1f94c44af83

---
+++ trunk/memoriavirtualEJB/ejbModule/br/usp/memoriavirtual/modelo/entidades/Autor.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package br.usp.memoriavirtual.modelo.entidades;
+
+/**
+ * @author bigmac
+ *
+ */
+public class Autor {
+
+	/**
+	 * 
+	 */
+	public Autor() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
