6150d8f080dd527e22c9967776e2e7028fd1e4f6
baldacim <baldacim@699894a6-35f8-415d-b816-a1f94c44af83>
2012-05-15 21:55:48 +0000

    git-svn-id: file:///home/brauner/Desktop/DEV_DUMP_19.05.2015/svn_storage/memoria_virtual_src@743 699894a6-35f8-415d-b816-a1f94c44af83

--- trunk/memoriavirtualEJB/ejbModule/br/usp/memoriavirtual/modelo/fachadas/CadastrarUsuario.java
+++ trunk/memoriavirtualEJB/ejbModule/br/usp/memoriavirtual/modelo/fachadas/CadastrarUsuario.java
@@ -1,11 +1,15 @@
 package br.usp.memoriavirtual.modelo.fachadas;
 
+import java.util.List;
+import java.util.logging.Logger;
+
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.NoResultException;
 import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
 
+import br.usp.memoriavirtual.modelo.entidades.Acesso;
 import br.usp.memoriavirtual.modelo.entidades.Usuario;
 import br.usp.memoriavirtual.modelo.fachadas.remoto.CadastrarUsuarioRemote;
 
@@ -31,25 +35,44 @@ public class CadastrarUsuario implements CadastrarUsuarioRemote {
 		return usuarioAutenticado;
 	}
 
+	
+	@SuppressWarnings("unchecked")
 	public void cadastrarUsuario(Usuario usuario, String validacao) throws ModeloException {
 
 		/* Busco pelo convite usando o campo ID na tabela */
-		Query query = this.entityManager.createQuery("SELECT u FROM Usuario u WHERE u.id = :usuario");
-		query.setParameter("usuario", validacao);
-
+		Query queryUsuario = this.entityManager.createQuery("SELECT u FROM Usuario u WHERE u.id = :usuario");
+		queryUsuario.setParameter("usuario", validacao);
+		
 		Usuario convite = null;
-
 		/*
 		 * Insiro no banco de dados uma nova tupla com os dados cadastrados e deleto a tupla usada para enviar o convite
 		 */
 		try {
-			convite = (Usuario) query.getSingleResult();
+			convite = (Usuario) queryUsuario.getSingleResult();
 		} catch (NoResultException e) {
 			throw new ModeloException("Convite invalido", e);
 		}
+		List<Acesso> acessos;
+		
+		Query queryAcesso = this.entityManager.createQuery("SELECT a FROM Acesso a Where a.usuario = :convite");
+		queryAcesso.setParameter("convite", convite);
+		
+		acessos = (List<Acesso>) queryAcesso.getResultList();
+		
+		for(Acesso acesso:acessos){
+			entityManager.remove(acesso);
+		}
+		
 		usuario.setAtivo(true);
 		usuario.setAdministrador(convite.isAdministrador());
-		entityManager.persist(usuario);
+		convite.setEmail("");
 		entityManager.remove(convite);
+		entityManager.persist(usuario);
+
+		
+		for(Acesso acesso:acessos){
+			Acesso novoAcesso = new Acesso(usuario, acesso.getInstituicao(), acesso.getGrupo());
+			entityManager.persist(novoAcesso);
+		}
 	}
 }
