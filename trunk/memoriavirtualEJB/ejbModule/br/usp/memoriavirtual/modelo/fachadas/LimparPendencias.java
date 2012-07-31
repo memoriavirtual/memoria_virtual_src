f197049a407084c0004c0aca1af6eb6931e54328
baldacim <baldacim@699894a6-35f8-415d-b816-a1f94c44af83>
2012-07-31 04:55:25 +0000

    git-svn-id: file:///home/brauner/Desktop/DEV_DUMP_19.05.2015/svn_storage/memoria_virtual_src@818 699894a6-35f8-415d-b816-a1f94c44af83

--- trunk/memoriavirtualEJB/ejbModule/br/usp/memoriavirtual/modelo/fachadas/LimparPendencias.java
+++ trunk/memoriavirtualEJB/ejbModule/br/usp/memoriavirtual/modelo/fachadas/LimparPendencias.java
@@ -2,6 +2,7 @@ package br.usp.memoriavirtual.modelo.fachadas;
 
 import java.lang.reflect.Constructor;
 import java.util.Collection;
+import java.util.Date;
 import java.util.List;
 import java.util.StringTokenizer;
 import java.util.logging.Logger;
@@ -20,8 +21,8 @@ import javax.persistence.Query;
 import br.usp.memoriavirtual.modelo.comandos.Comando;
 import br.usp.memoriavirtual.modelo.comandos.ControleComandos;
 import br.usp.memoriavirtual.modelo.entidades.Aprovacao;
-import br.usp.memoriavirtual.modelo.fachadas.remoto.MemoriaVirtualRemote;
 import br.usp.memoriavirtual.modelo.fachadas.remoto.LimparPendenciasrRemote;
+import br.usp.memoriavirtual.modelo.fachadas.remoto.MemoriaVirtualRemote;
 
 
 @Singleton
@@ -69,8 +70,18 @@ public class LimparPendencias implements LimparPendenciasrRemote {
 		
 		@SuppressWarnings("unchecked")
 		List<Aprovacao> aprovacoes = (List<Aprovacao>) query.getResultList();
+		
 		for(Aprovacao aprov : aprovacoes){
 			
+			/*Atualiza o valor que está no banco de dado*/
+			entityManager.refresh(aprov);
+			
+			/*Cria a data atual e verifica se já expirou a data da aprovação*/
+			Date dataAtual = new Date();
+			if(dataAtual.before(aprov.getExpiracao()))
+				continue;
+			
+			/*Extraimos o nome da Entidade que gerou a dependencia*/
 			String name = aprov.getTabelaEstrangeira();
 			
 			StringTokenizer strToken = new StringTokenizer(name, ".");
@@ -78,9 +89,14 @@ public class LimparPendencias implements LimparPendenciasrRemote {
 				name = strToken.nextToken();
 			}
 			
+			/*Tenta-se criar um comando baseado na entidade que gerou a dependencia*/
 			try {
-				Class<?> cls = Class.forName(this.getClass().getPackage() + name);
+				/*Esse eh o package onde se encontra as implementacoes da interface comandos*/
+				String nomeDaDependencia = "br.usp.memoriavirtual.modelo.comandos." + name;
 				
+				/*Aqui utiliza-se Java Reflection para criar a instancia de uma classe escolhida dinamicamente*/
+				Class<?> cls = Class.forName(nomeDaDependencia);
+
 				Class<?> parameter[] = new Class[1];  
 	            parameter[0] = Aprovacao.class;  
 	           
@@ -91,17 +107,17 @@ public class LimparPendencias implements LimparPendenciasrRemote {
 	            
 	            Object retobj = ct.newInstance(arglist);  
 	            
+	            /*Adiciona-se na lista de comandos a ser executados para exclusão da dependencia*/
 	            controle.adicionar((Comando)retobj);
 	            
 			} catch (Exception e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
-			}	
+			}
 		}
 		
 		controle.executeAll();
-		
-		//limpezaFabrica.limparConvites();
+
 	}
 
 	
@@ -111,8 +127,8 @@ public class LimparPendencias implements LimparPendenciasrRemote {
 		/*Cria o primeiro timer para expirar em 2 segundos*/
 		this.criarTimer(2000);
 
-		/*	Pego a lista de Timers criados para est� classe e cancelo o timer criado
-		 * pelo container EJB via anota��es.
+		/*	Pego a lista de Timers criados para est� classe e cancelo o timer criado
+		 * pelo container EJB via anotações.
 		 */
 		Collection<javax.ejb.Timer> timerList = timerService.getTimers();
 		if(!timerList.isEmpty()){
