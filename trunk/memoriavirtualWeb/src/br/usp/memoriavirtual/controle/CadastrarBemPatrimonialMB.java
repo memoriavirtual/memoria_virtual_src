5cdeccf9c4590c9c139cdce68cce76361e172bd0
diegogomes <diegogomes@699894a6-35f8-415d-b816-a1f94c44af83>
2012-09-06 13:57:24 +0000

    git-svn-id: file:///home/brauner/Desktop/DEV_DUMP_19.05.2015/svn_storage/memoria_virtual_src@852 699894a6-35f8-415d-b816-a1f94c44af83

--- trunk/memoriavirtualWeb/src/br/usp/memoriavirtual/controle/CadastrarBemPatrimonialMB.java
+++ trunk/memoriavirtualWeb/src/br/usp/memoriavirtual/controle/CadastrarBemPatrimonialMB.java
@@ -66,7 +66,38 @@ public class CadastrarBemPatrimonialMB implements BeanComMidia, Serializable {
 	protected String producaoAno;
 	protected String producaoEdicao;
 	protected String producaoOutrasRes;
-
+	protected String caracteristicasFisicas;
+	protected String condicaoTopografica;
+	protected String uso;
+	protected Integer numAmbientes;
+	protected Integer numPavimentos;
+	protected Boolean  alcova;
+	protected Boolean porao;
+	protected Boolean sotao;
+	protected String descricaoOutros;
+	protected String relevo;
+	protected String caracteristicasAmbientais;
+	protected String caracteristicasAntropico;
+	protected String sitioDaPaisagem;
+	protected String aguaProxima;
+	protected String possuiVegetacao;
+	protected String exposicao;
+	protected String usoAtual;
+	protected String descricaoNotas;
+	protected String dimensoesEQuantificacoes;
+	protected Double areaTotal;
+	protected Double alturaFachadaFrontal;
+	protected Double alturaFachadaSuperior;
+	protected Double largura;
+	protected Double profundidade;
+	protected Double alturaDaCumeeira;
+	protected Double alturaTotal;
+	protected Double peDireitoTerreo;
+	protected String peDireitoTipo;
+	protected Double comprimento;
+	protected Double altura;
+	protected String conteudo;
+	protected String meioDeAcesso;
 	/**
 	 * 
 	 */
@@ -481,6 +512,202 @@ public class CadastrarBemPatrimonialMB implements BeanComMidia, Serializable {
 
 	public void setProducaoOutrasRes(String producaoOutrasRes) {
 		this.producaoOutrasRes = producaoOutrasRes;
+	}
+
+	public String getCaracteristicasFisicas() {
+		return caracteristicasFisicas;
+	}
+	public void setCaracteristicasFisicas(String caracteristicasFisicas) {
+		this.caracteristicasFisicas = caracteristicasFisicas;
+	}
+	public String getCondicaoTopografica() {
+		return condicaoTopografica;
+	}
+	public void setCondicaoTopografica(String condicaoTopografica) {
+		this.condicaoTopografica = condicaoTopografica;
+	}
+	public String getUso() {
+		return uso;
+	}
+	public void setUso(String uso) {
+		this.uso = uso;
+	}
+	public Integer getNumAmbientes() {
+		return numAmbientes;
+	}
+	public void setNumAmbientes(Integer numAmbientes) {
+		this.numAmbientes = numAmbientes;
+	}
+	public Integer getNumPavimentos() {
+		return numPavimentos;
+	}
+	public void setNumPavimentos(Integer numPavimentos) {
+		this.numPavimentos = numPavimentos;
+	}
+	public Boolean getAlcova() {
+		return alcova;
+	}
+	public void setAlcova(Boolean alcova) {
+		this.alcova = alcova;
+	}
+	public Boolean getPorao() {
+		return porao;
+	}
+	public void setPorao(Boolean porao) {
+		this.porao = porao;
+	}
+	public Boolean getSotao() {
+		return sotao;
+	}
+	public void setSotao(Boolean sotao) {
+		this.sotao = sotao;
+	}
+	public String getDescricaoOutros() {
+		return descricaoOutros;
+	}
+	public void setDescricaoOutros(String descricaoOutros) {
+		this.descricaoOutros = descricaoOutros;
+	}
+	public String getRelevo() {
+		return relevo;
+	}
+	public void setRelevo(String relevo) {
+		this.relevo = relevo;
+	}
+	public String getCaracteristicasAmbientais() {
+		return caracteristicasAmbientais;
+	}
+	public void setCaracteristicasAmbientais(String caracteristicasAmbientais) {
+		this.caracteristicasAmbientais = caracteristicasAmbientais;
+	}
+	public String getCaracteristicasAntropico() {
+		return caracteristicasAntropico;
+	}
+	public void setCaracteristicasAntropico(String caracteristicasAntropico) {
+		this.caracteristicasAntropico = caracteristicasAntropico;
+	}
+	public String getSitioDaPaisagem() {
+		return sitioDaPaisagem;
+	}
+	public void setSitioDaPaisagem(String sitioDaPaisagem) {
+		this.sitioDaPaisagem = sitioDaPaisagem;
+	}
+	public String getAguaProxima() {
+		return aguaProxima;
+	}
+	public void setAguaProxima(String aguaProxima) {
+		this.aguaProxima = aguaProxima;
+	}
+	public String getPossuiVegetacao() {
+		return possuiVegetacao;
+	}
+	public void setPossuiVegetacao(String possuiVegetacao) {
+		this.possuiVegetacao = possuiVegetacao;
+	}
+	public String getExposicao() {
+		return exposicao;
+	}
+	public void setExposicao(String exposicao) {
+		this.exposicao = exposicao;
+	}
+	public String getUsoAtual() {
+		return usoAtual;
+	}
+	public void setUsoAtual(String usoAtual) {
+		this.usoAtual = usoAtual;
+	}
+	public String getDescricaoNotas() {
+		return descricaoNotas;
+	}
+	public void setDescricaoNotas(String descricaoNotas) {
+		this.descricaoNotas = descricaoNotas;
+	}
+	public String getDimensoesEQuantificacoes() {
+		return dimensoesEQuantificacoes;
+	}
+	public void setDimensoesEQuantificacoes(String dimensoesEQuantificacoes) {
+		this.dimensoesEQuantificacoes = dimensoesEQuantificacoes;
+	}
+	public Double getAreaTotal() {
+		return areaTotal;
+	}
+	public void setAreaTotal(Double areaTotal) {
+		this.areaTotal = areaTotal;
+	}
+	public Double getAlturaFachadaFrontal() {
+		return alturaFachadaFrontal;
+	}
+	public void setAlturaFachadaFrontal(Double alturaFachadaFrontal) {
+		this.alturaFachadaFrontal = alturaFachadaFrontal;
+	}
+	public Double getAlturaFachadaSuperior() {
+		return alturaFachadaSuperior;
+	}
+	public void setAlturaFachadaSuperior(Double alturaFachadaSuperior) {
+		this.alturaFachadaSuperior = alturaFachadaSuperior;
+	}
+	public Double getLargura() {
+		return largura;
+	}
+	public void setLargura(Double largura) {
+		this.largura = largura;
+	}
+	public Double getProfundidade() {
+		return profundidade;
+	}
+	public void setProfundidade(Double profundidade) {
+		this.profundidade = profundidade;
+	}
+	public Double getAlturaDaCumeeira() {
+		return alturaDaCumeeira;
+	}
+	public void setAlturaDaCumeeira(Double alturaDaCumeeira) {
+		this.alturaDaCumeeira = alturaDaCumeeira;
+	}
+	public Double getAlturaTotal() {
+		return alturaTotal;
+	}
+	public void setAlturaTotal(Double alturaTotal) {
+		this.alturaTotal = alturaTotal;
+	}
+	public Double getPeDireitoTerreo() {
+		return peDireitoTerreo;
+	}
+	public void setPeDireitoTerreo(Double peDireitoTerreo) {
+		this.peDireitoTerreo = peDireitoTerreo;
+	}
+	public String getPeDireitoTipo() {
+		return peDireitoTipo;
+	}
+	public void setPeDireitoTipo(String peDireitoTipo) {
+		this.peDireitoTipo = peDireitoTipo;
+	}
+	public Double getComprimento() {
+		return comprimento;
+	}
+	public void setComprimento(Double comprimento) {
+		this.comprimento = comprimento;
+	}
+	public Double getAltura() {
+		return altura;
+	}
+	public void setAltura(Double altura) {
+		this.altura = altura;
+	}
+	public String getConteudo() {
+		return conteudo;
+	}
+	public void setConteudo(String conteudo) {
+		this.conteudo = conteudo;
+	}
+	public String getMeioDeAcesso() {
+		return meioDeAcesso;
+	}
+	public void setMeioDeAcesso(String meioDeAcesso) {
+		this.meioDeAcesso = meioDeAcesso;
+	}
+	public static long getSerialversionuid() {
+		return serialVersionUID;
 	}
 
 	public class ApresentaAutoria implements Serializable {
