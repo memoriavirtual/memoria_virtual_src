8a3672aae99be22e0389a932840c81966aac2f38
joesantos <joesantos@699894a6-35f8-415d-b816-a1f94c44af83>
2012-12-02 17:52:42 +0000

    git-svn-id: file:///home/brauner/Desktop/DEV_DUMP_19.05.2015/svn_storage/memoria_virtual_src@875 699894a6-35f8-415d-b816-a1f94c44af83

--- trunk/memoriavirtualEJB/ejbModule/br/usp/memoriavirtual/modelo/fachadas/RealizarBuscaSimples.java
+++ trunk/memoriavirtualEJB/ejbModule/br/usp/memoriavirtual/modelo/fachadas/RealizarBuscaSimples.java
@@ -13,32 +13,50 @@ import com.sun.tools.ws.processor.modeler.ModelerException;
 import br.usp.memoriavirtual.modelo.entidades.bempatrimonial.BemPatrimonial;
 import br.usp.memoriavirtual.modelo.fachadas.remoto.RealizarBuscaSimplesRemote;
 
-@Stateless(mappedName="RealizarBuscaSimples")
+@Stateless(mappedName = "RealizarBuscaSimples")
 public class RealizarBuscaSimples implements RealizarBuscaSimplesRemote {
 
 	@PersistenceContext(unitName = "memoriavirtual")
 	private EntityManager entityManager;
 
-	@SuppressWarnings("unchecked")
 	@Override
 	public List<BemPatrimonial> buscar(String busca) throws ModeloException {
-		
+
 		List<BemPatrimonial> bens = new ArrayList<BemPatrimonial>();
 		List<String> stringsDeBusca = new ArrayList<String>();
 		Query query;
-		
-		for(String s : stringsDeBusca){
-			try{
-				query = (Query) entityManager.createQuery("SELECT b FROM BemPatrimonial b");
-				bens = (List<BemPatrimonial>)query.getResultList();
-			}
-			catch(Exception e){
+		stringsDeBusca = obterStrings(busca);
+
+		/*for (String s : stringsDeBusca) {
+			try {
+				query = (Query) entityManager
+						.createQuery("SELECT b FROM BemPatrimonial b");
+				bens = (List<BemPatrimonial>) query.getResultList();
+			} catch (Exception e) {
 				throw new ModeloException(e);
 			}
+		}*/
+
+		return bens;
+	}
+
+	public List<String> obterStrings(String busca) {
+
+		List<String> lista = new ArrayList<String>();
+		List<String> retorno = new ArrayList<String>();
+		String[] strings = busca.split(" ");
+		int nroEspacos = 0;
+
+		for (String s : strings){
+			lista.add(s);
+			nroEspacos++;
 		}
 		
 		
-		return bens;
+		
+		
+
+		return lista;
 	}
-	
+
 }
