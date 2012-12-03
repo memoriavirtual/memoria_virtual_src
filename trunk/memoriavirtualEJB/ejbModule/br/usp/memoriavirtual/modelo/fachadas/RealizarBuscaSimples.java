b4e0b22a069ac37a28f883f4a11e63fb0525fda4
joesantos <joesantos@699894a6-35f8-415d-b816-a1f94c44af83>
2012-12-03 22:58:09 +0000

    git-svn-id: file:///home/brauner/Desktop/DEV_DUMP_19.05.2015/svn_storage/memoria_virtual_src@877 699894a6-35f8-415d-b816-a1f94c44af83

--- trunk/memoriavirtualEJB/ejbModule/br/usp/memoriavirtual/modelo/fachadas/RealizarBuscaSimples.java
+++ trunk/memoriavirtualEJB/ejbModule/br/usp/memoriavirtual/modelo/fachadas/RealizarBuscaSimples.java
@@ -8,8 +8,6 @@ import javax.persistence.Query;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
-import com.sun.tools.ws.processor.modeler.ModelerException;
-
 import br.usp.memoriavirtual.modelo.entidades.bempatrimonial.BemPatrimonial;
 import br.usp.memoriavirtual.modelo.fachadas.remoto.RealizarBuscaSimplesRemote;
 
@@ -19,23 +17,54 @@ public class RealizarBuscaSimples implements RealizarBuscaSimplesRemote {
 	@PersistenceContext(unitName = "memoriavirtual")
 	private EntityManager entityManager;
 
+	@SuppressWarnings("unchecked")
 	@Override
 	public List<BemPatrimonial> buscar(String busca) throws ModeloException {
 
 		List<BemPatrimonial> bens = new ArrayList<BemPatrimonial>();
+		List<BemPatrimonial> parcial = new ArrayList<BemPatrimonial>();
 		List<String> stringsDeBusca = new ArrayList<String>();
 		Query query;
 		stringsDeBusca = obterStrings(busca);
 
-		/*for (String s : stringsDeBusca) {
+		for (String s : stringsDeBusca) {
 			try {
-				query = (Query) entityManager
-						.createQuery("SELECT b FROM BemPatrimonial b");
-				bens = (List<BemPatrimonial>) query.getResultList();
+				query = entityManager
+						.createQuery("SELECT b FROM BemPatrimonial b, "
+								+ "BemPatrimonial_Titulos t WHERE "
+								+ "t.bempatrimonial_id = b.id AND t.valor LIKE :padrao");
+				query.setParameter("padrao", "%" + s + "%");
+				parcial = (List<BemPatrimonial>) query.getResultList();
+				for (BemPatrimonial b : parcial) {
+					if (!bens.contains(b))
+						bens.add(b);
+				}
+
+				query = entityManager
+						.createQuery("SELECT b FROM BemPatrimonial b, "
+								+ "BemPatrimonial_Descritores d WHERE "
+								+ "d.bempatrimonial_id = b.id AND d.descritores LIKE :padrao");
+				query.setParameter("padrao", "%" + s + "%");
+				parcial = (List<BemPatrimonial>) query.getResultList();
+				for (BemPatrimonial b : parcial) {
+					if (!bens.contains(b))
+						bens.add(b);
+				}
+
+				query = entityManager
+						.createQuery("SELECT b FROM BemPatrimonial b, Autoria au, Autor a "
+								+ "where b.id = au.bempatrimonial_id AND"
+								+ "au.autor_id = a.id AND a.nome LIKE :padrao");
+				query.setParameter("padrao", "%" + s + "%");
+				parcial = (List<BemPatrimonial>) query.getResultList();
+				for (BemPatrimonial b : parcial) {
+					if (!bens.contains(b))
+						bens.add(b);
+				}
 			} catch (Exception e) {
 				throw new ModeloException(e);
 			}
-		}*/
+		}
 
 		return bens;
 	}
@@ -47,14 +76,10 @@ public class RealizarBuscaSimples implements RealizarBuscaSimplesRemote {
 		String[] strings = busca.split(" ");
 		int nroEspacos = 0;
 
-		for (String s : strings){
+		for (String s : strings) {
 			lista.add(s);
 			nroEspacos++;
 		}
-		
-		
-		
-		
 
 		return lista;
 	}
