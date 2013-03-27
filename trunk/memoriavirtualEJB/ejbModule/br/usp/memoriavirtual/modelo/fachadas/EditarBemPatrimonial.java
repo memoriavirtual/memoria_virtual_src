4bae9afe6d5acf227b0c52aa9a9afe597175bcbd
diegogomes <diegogomes@699894a6-35f8-415d-b816-a1f94c44af83>
2013-03-27 14:46:22 +0000

    git-svn-id: file:///home/brauner/Desktop/DEV_DUMP_19.05.2015/svn_storage/memoria_virtual_src@917 699894a6-35f8-415d-b816-a1f94c44af83

--- trunk/memoriavirtualEJB/ejbModule/br/usp/memoriavirtual/modelo/fachadas/EditarBemPatrimonial.java
+++ trunk/memoriavirtualEJB/ejbModule/br/usp/memoriavirtual/modelo/fachadas/EditarBemPatrimonial.java
@@ -10,9 +10,6 @@ import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
 
-import br.usp.memoriavirtual.modelo.entidades.bempatrimonial.BemArqueologico;
-import br.usp.memoriavirtual.modelo.entidades.bempatrimonial.BemArquitetonico;
-import br.usp.memoriavirtual.modelo.entidades.bempatrimonial.BemNatural;
 import br.usp.memoriavirtual.modelo.entidades.bempatrimonial.BemPatrimonial;
 import br.usp.memoriavirtual.modelo.fachadas.remoto.EditarBemPatrimonialRemote;
 
@@ -29,7 +26,7 @@ public class EditarBemPatrimonial implements EditarBemPatrimonialRemote {
 	
 	@PersistenceContext(unitName = "memoriavirtual")
 	private EntityManager entityManager;
-
+	
 	@SuppressWarnings("unchecked")
 	@Override
 	public List<BemPatrimonial> listarBensPatrimoniais (String strDeBusca) throws ModeloException{
@@ -49,157 +46,9 @@ public class EditarBemPatrimonial implements EditarBemPatrimonialRemote {
 	@Override
 	public void salvarBemPatrimonial(BemPatrimonial bem) throws ModeloException {
 		
+		//Merge do bem patrimonial
+		entityManager.merge(bem); 
 		
-		BemPatrimonial bemOld;
-		
-		bemOld = this.entityManager.find(BemPatrimonial.class,  bem.getId());
-		
-		if(bemOld != null){
-			try {
-				bemOld.setInstituicao(bem.getInstituicao());
-			
-			if (bem.getTipoDoBemPatrimonial().equals(new String(BemPatrimonial.TipoDoBemPatrimonial.ARQUEOLOGICO.name()))) {
-				// System.out.println("arqueologico");
-				((BemArqueologico)bemOld).setCondicaoTopografica(((BemArqueologico)bem).getCondicaoTopografica());
-				((BemArqueologico)bemOld).setSitioDaPaisagem(((BemArqueologico)bem).getSitioDaPaisagem());
-				((BemArqueologico)bemOld).setAguaProximo(((BemArqueologico)bem).getAguaProximo());
-				((BemArqueologico)bemOld).setPossuiVegetacao(((BemArqueologico)bem).getPossuiVegetacao());
-				((BemArqueologico)bemOld).setExposicao(((BemArqueologico)bem).getExposicao());
-				((BemArqueologico)bemOld).setUsoAtual(((BemArqueologico)bem).getUsoAtual());
-				((BemArqueologico)bemOld).setOutros(((BemArqueologico)bem).getOutros());
-				((BemArqueologico)bemOld).setNotas(((BemArqueologico)bem).getNotas());
-				((BemArqueologico)bemOld).setAreaTotal(((BemArqueologico)bem).getAreaTotal());
-				((BemArqueologico)bemOld).setComprimento(((BemArqueologico)bem).getComprimento());
-				((BemArqueologico)bemOld).setAltura(((BemArqueologico)bem).getAltura());
-				((BemArqueologico)bemOld).setLargura(((BemArqueologico)bem).getLargura());
-				((BemArqueologico)bemOld).setProfundidade(((BemArqueologico)bem).getProfundidade());
-						  
-						
-				((BemArqueologico)bemOld).getDiagnostico().setEstConservacao(((BemArqueologico)bem).getDiagnostico().getEstConservacao());
-				((BemArqueologico)bemOld).getDiagnostico().setNotaEstConservacao(((BemArqueologico)bem).getDiagnostico().getNotaEstConservacao());
-				
-				
-			} else if (bem.getTipoDoBemPatrimonial().equals(new String(BemPatrimonial.TipoDoBemPatrimonial.ARQUITETONICO.name()))) {
-				// System.out.println("edificado");
-				((BemArquitetonico)bemOld).setProfundidade(((BemArquitetonico)bem).getProfundidade());
-				((BemArquitetonico)bemOld).setCondicaoTopografia(((BemArquitetonico)bem).getCondicaoTopografia());
-				((BemArquitetonico)bemOld).setUso(((BemArquitetonico)bem).getUso());
-				((BemArquitetonico)bemOld).setNumeroDePavimentos(((BemArquitetonico)bem).getNumeroDePavimentos());
-				((BemArquitetonico)bemOld).setNumeroDeAmbientes(((BemArquitetonico)bem).getNumeroDeAmbientes());
-				((BemArquitetonico)bemOld).setAlcova(((BemArquitetonico)bem).getAlcova());
-				((BemArquitetonico)bemOld).setPorao(((BemArquitetonico)bem).getPorao());
-				((BemArquitetonico)bemOld).setSotao(((BemArquitetonico)bem).getSotao());
-				((BemArquitetonico)bemOld).setOutros(((BemArquitetonico)bem).getOutros());
-				((BemArquitetonico)bemOld).setAreaTotal(((BemArquitetonico)bem).getAreaTotal());
-				((BemArquitetonico)bemOld).setAlturaFachFrontal(((BemArquitetonico)bem).getAlturaFachFrontal());
-				((BemArquitetonico)bemOld).setAlturaFachPosterior(((BemArquitetonico)bem).getAlturaFachPosterior());
-				((BemArquitetonico)bemOld).setLargura(((BemArquitetonico)bem).getLargura());
-				((BemArquitetonico)bemOld).setAlturaTotal(((BemArquitetonico)bem).getAlturaTotal());
-				((BemArquitetonico)bemOld).setPeDireitoTerreo(((BemArquitetonico)bem).getPeDireitoTerreo());	
-				((BemArquitetonico)bemOld).setTipoPeDireito(((BemArquitetonico)bem).getTipoPeDireito());
-				
-				
-				((BemArquitetonico)bemOld).getDiagnostico().setEstConservacao(((BemArquitetonico)bem).getDiagnostico().getEstConservacao());
-				((BemArquitetonico)bemOld).getDiagnostico().setEstPreservacao(((BemArquitetonico)bem).getDiagnostico().getEstPreservacao());
-				((BemArquitetonico)bemOld).getDiagnostico().setNotaEstConservacao(((BemArquitetonico)bem).getDiagnostico().getNotaEstConservacao());
-						
-			} else if (bem.getTipoDoBemPatrimonial().equals(new String(BemPatrimonial.TipoDoBemPatrimonial.NATURAL.name()))) {
-				// System.out.println("natural");
-				((BemNatural)bemOld).setRelevo(((BemNatural)bem).getRelevo());
-				((BemNatural)bemOld).setMeioAntropico(((BemNatural)bem).getMeioAntropico());
-				((BemNatural)bemOld).setCaracteristicasAmbientais(((BemNatural)bem).getCaracteristicasAmbientais());
-				((BemNatural)bemOld).setCaracteristicasAmbientais(((BemNatural)bem).getCaracteristicasAmbientais());
-				
-						
-				((BemNatural)bemOld).getDiagnostico().setEstPreservacao(((BemNatural)bem).getDiagnostico().getEstPreservacao());
-				((BemNatural)bemOld).getDiagnostico().setNotaEstConservacao(((BemNatural)bem).getDiagnostico().getNotaEstConservacao());
-			} else {
-				
-				bemOld.getDiagnostico().setEstPreservacao(bem.getDiagnostico().getEstPreservacao());
-				bemOld.getDiagnostico().setNotaEstConservacao(bem.getDiagnostico().getNotaEstConservacao());
-			}
-
-			//anexando Geral Info
-
-			
-			bemOld.getTitulos().clear();
-			bemOld.getTitulos().addAll(bem.getTitulos());
-			bemOld.setColecao(bem.getColecao());
-			bemOld.setExterno(bem.isExterno());
-			bemOld.setLatitude(bem.getLatitude());
-			bemOld.setNumeroDeRegistro(bem.getNumeroDeRegistro());
-			bemOld.setLongitude(bem.getLongitude());
-			bemOld.setComplemento(bem.getComplemento());
-			// fim Geral info
-
-			// anexando autorias
-			bemOld.getAutorias().clear();
-			bemOld.getAutorias().addAll(bem.getAutorias());
-			// fim autorias
-			// anexando produ��o
-
-			bemOld.setProducao(bem.getProducao());
-			
-			// fim anexando produ��o
-			// System.out.println("normal");
-			// anexando descri��o
-			bemOld
-					.setCaracteristicasFisTecExec(bem.getCaracteristicasFisTecExec());
-
-			// fim anexando descri��o
-
-			// anexando Intervencao e diagnostico
-			bemOld.getIntervencoes().clear();
-			bemOld.setIntervencoes(bem.getIntervencoes());
-			bemOld.setDiagnostico(bem.getDiagnostico());
-			// fim anexando intervencao e diagnostico
-			// anexando Disponibilidade Uso e Protec�o
-
-			bemOld
-					.setDisponibilidadeUsoProtecao(bem.getDisponibilidadeUsoProtecao());
-			// Fim anexando Disponibilidade Uso e Protec�o
-			// anexado historio e procerdencia
-
-			bemOld
-					.setHisttoricoProcedencia(bem.getHisttoricoProcedencia());
-
-			// fim anexando historico e procedencia
-
-			// anexando assuntos
-			bemOld.getAssuntos().clear();
-			bemOld.setAssuntos(bem.getAssuntos());
-			// fim assuntos
-
-			// anexando descritores
-			bemOld.getDescritores().clear();
-			bemOld.setDescritores(bem.getDescritores());
-			// fim descritores
-
-			// adcionando fontes de informa��o
-			bemOld.setFontesInformacao(bem.getFontesInformacao());
-			// fim adcionando fontes de informa��o
-			bemOld.getPesquisadores().clear();
-			bemOld.setPesquisadores(bem.getPesquisadores());
-			
-			bemOld.getContainerMultimidia().getMultimidia().clear();
-			bemOld.getContainerMultimidia().setMultimidia(bem.getContainerMultimidia().getMultimidia());
-			
-			
-			
-			
-			
-				
-			} catch (Exception e) {
-				throw new ModeloException(e);
-				
-			}
-		}
-		
-		try {
-			entityManager.flush();
-		} catch (Exception t) {
-			throw new ModeloException(t);
-		}
 	}
 	
 
