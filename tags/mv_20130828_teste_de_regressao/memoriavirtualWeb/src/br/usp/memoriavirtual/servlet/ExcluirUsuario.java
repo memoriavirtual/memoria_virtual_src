9ddaab642504e0eba1c8bd9db9e08b1568c1fd16
joesantos <joesantos@699894a6-35f8-415d-b816-a1f94c44af83>
2013-08-28 18:30:57 +0000

    Nova versão do sistema
    git-svn-id: file:///home/brauner/Desktop/DEV_DUMP_19.05.2015/svn_storage/memoria_virtual_src@1060 699894a6-35f8-415d-b816-a1f94c44af83

---
+++ tags/mv_20130828_teste_de_regressao/memoriavirtualWeb/src/br/usp/memoriavirtual/servlet/ExcluirUsuario.java
@@ -0,0 +1,100 @@
+package br.usp.memoriavirtual.servlet;
+
+import java.io.IOException;
+
+import javax.ejb.EJB;
+import javax.el.ELResolver;
+import javax.faces.context.FacesContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import br.usp.memoriavirtual.controle.ExcluirUsuarioMB;
+import br.usp.memoriavirtual.modelo.entidades.Usuario;
+import br.usp.memoriavirtual.modelo.fachadas.ModeloException;
+import br.usp.memoriavirtual.modelo.fachadas.remoto.EditarCadastroUsuarioRemote;
+import br.usp.memoriavirtual.modelo.fachadas.remoto.ExcluirUsuarioRemote;
+import br.usp.memoriavirtual.modelo.fachadas.remoto.MemoriaVirtualRemote;
+import br.usp.memoriavirtual.utils.FacesUtil;
+
+/**
+ * Servlet implementation class ExcluirUsuario
+ */
+@WebServlet("/excluirusuario")
+public class ExcluirUsuario extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	@EJB
+	private MemoriaVirtualRemote memoriaVirtualEJB;
+	@EJB
+	private ExcluirUsuarioRemote excluirUsuarioEJB;
+	@EJB
+	private EditarCadastroUsuarioRemote editarCadastroUsuarioEJB;
+
+	public ExcluirUsuario() {
+		super();
+	}
+
+	protected void doGet(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+
+		String aprovacaoId = request.getParameter("aprovacao");
+		aprovacaoId = this.memoriaVirtualEJB.embaralhar(aprovacaoId);
+
+		String usuarioNome = request.getParameter("usuario");
+		usuarioNome = this.memoriaVirtualEJB.embaralhar(usuarioNome);
+
+		Usuario usuario = (Usuario) request.getSession()
+				.getAttribute("usuario");
+
+		if (!this.editarCadastroUsuarioEJB.isAprovador(usuario, aprovacaoId)) {
+			response.sendRedirect("restrito/excluirusuario_etapa1.jsf");
+			return;
+		}
+
+		boolean expiracao = false;
+
+		try {
+			expiracao = editarCadastroUsuarioEJB
+					.isAprovacaoExpirada(aprovacaoId);
+		} catch (ModeloException m) {
+			m.printStackTrace();
+		}
+
+		if (expiracao) {
+
+			try {
+				editarCadastroUsuarioEJB.removerAprovacao(aprovacaoId);
+
+				Usuario pendente = excluirUsuarioEJB
+						.recuperarDadosUsuario(usuarioNome);
+
+				excluirUsuarioEJB.marcarUsuarioExcluido(pendente, true, true);
+			} catch (ModeloException m) {
+				m.printStackTrace();
+			}
+
+		}
+
+		else {
+
+			// Inicializando Managed Bean
+			FacesContext facesContext = FacesUtil.getFacesContext(request,
+					response);
+			ELResolver resolver = facesContext.getApplication().getELResolver();
+			ExcluirUsuarioMB managedBean = (ExcluirUsuarioMB) resolver
+					.getValue(facesContext.getELContext(), null,
+							"excluirUsuarioMB");
+
+			managedBean.setAprovacao(aprovacaoId);
+			managedBean.setNome(usuarioNome);
+			managedBean.setUsuario(null);
+			managedBean.listarAcessos();
+
+			response.sendRedirect("restrito/confirmarexclusao.jsf");
+		}
+
+	}
+
+}
