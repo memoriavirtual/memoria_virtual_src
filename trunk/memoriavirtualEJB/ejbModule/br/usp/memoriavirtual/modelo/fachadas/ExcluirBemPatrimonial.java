a66f111f904cc95076764262c4ee3a8a8eebbdbc
joesantos <joesantos@699894a6-35f8-415d-b816-a1f94c44af83>
2013-09-11 18:50:10 +0000

    resolvendo defeito #262
    git-svn-id: file:///home/brauner/Desktop/DEV_DUMP_19.05.2015/svn_storage/memoria_virtual_src@1069 699894a6-35f8-415d-b816-a1f94c44af83

--- trunk/memoriavirtualEJB/ejbModule/br/usp/memoriavirtual/modelo/fachadas/ExcluirBemPatrimonial.java
+++ trunk/memoriavirtualEJB/ejbModule/br/usp/memoriavirtual/modelo/fachadas/ExcluirBemPatrimonial.java
@@ -1,12 +1,8 @@
 package br.usp.memoriavirtual.modelo.fachadas;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
-import javax.persistence.Query;
 
 import br.usp.memoriavirtual.modelo.entidades.bempatrimonial.BemPatrimonial;
 import br.usp.memoriavirtual.modelo.fachadas.remoto.ExcluirBemPatrimonialRemote;
@@ -16,27 +12,6 @@ public class ExcluirBemPatrimonial implements ExcluirBemPatrimonialRemote {
 
 	@PersistenceContext(unitName = "memoriavirtual")
 	private EntityManager entityManager;
-
-	@SuppressWarnings("unchecked")
-	@Override
-	public List<BemPatrimonial> listarBensPatrimoniais(String nome)
-			throws ModeloException {
-
-		List<BemPatrimonial> bens = new ArrayList<BemPatrimonial>();
-
-		Query query = this.entityManager
-				.createQuery("SELECT t.bemPatrimonial FROM BEMPATRIMONIAL_TITULOS t WHERE  t.valor LIKE :nome");
-		query.setParameter("nome", "%" + nome + "%");
-
-		try {
-			bens = (List<BemPatrimonial>) query.getResultList();
-		} catch (Exception e) {
-			throw new ModeloException(e);
-		}
-
-		return bens;
-
-	}
 	
 	@Override
 	public BemPatrimonial recuperarDados(BemPatrimonial bem) throws ModeloException{
@@ -54,11 +29,6 @@ public class ExcluirBemPatrimonial implements ExcluirBemPatrimonialRemote {
 		try{
 			
 			BemPatrimonial excluido = this.entityManager.find(BemPatrimonial.class, bem.getId());
-			
-			Query query = this.entityManager.createQuery("DELETE FROM Titulo t WHERE t.bemPatrimonial = :bempatrimonial");
-			query.setParameter("bempatrimonial", excluido);
-			query.executeUpdate();
-
 			this.entityManager.remove(excluido);
 		}
 		catch(Exception e){
--- trunk/memoriavirtualEJB/ejbModule/br/usp/memoriavirtual/modelo/fachadas/remoto/ExcluirBemPatrimonialRemote.java
+++ trunk/memoriavirtualEJB/ejbModule/br/usp/memoriavirtual/modelo/fachadas/remoto/ExcluirBemPatrimonialRemote.java
@@ -1,7 +1,5 @@
 package br.usp.memoriavirtual.modelo.fachadas.remoto;
 
-import java.util.List;
-
 import javax.ejb.Remote;
 
 import br.usp.memoriavirtual.modelo.entidades.bempatrimonial.BemPatrimonial;
@@ -10,7 +8,6 @@ import br.usp.memoriavirtual.modelo.fachadas.ModeloException;
 @Remote
 public interface ExcluirBemPatrimonialRemote {
 	
-	public List<BemPatrimonial> listarBensPatrimoniais(String nome) throws ModeloException;
 	public BemPatrimonial recuperarDados(BemPatrimonial bem) throws ModeloException;
 	public void excluirBem(BemPatrimonial bem) throws ModeloException;
 
