ae6a02973aed580d8633de5ff9516738ef127c61
tbianchi <tbianchi@699894a6-35f8-415d-b816-a1f94c44af83>
2013-09-18 18:43:02 +0000

    Funcionalidade #249: Interfaces de busca REST e SOAP desenvolvidas. Resta resolver o problema de instanciação dos relacionamentos do tipo "Lazy" da entidade Bem Patrimonial.
    git-svn-id: file:///home/brauner/Desktop/DEV_DUMP_19.05.2015/svn_storage/memoria_virtual_src@1089 699894a6-35f8-415d-b816-a1f94c44af83

--- trunk/memoriavirtualWebService/src/br/usp/memoriavirtual/servicos/soap/RealizarBuscaSOAPService.java
+++ trunk/memoriavirtualWebService/src/br/usp/memoriavirtual/servicos/soap/RealizarBuscaSOAPService.java
@@ -1,15 +1,28 @@
 package br.usp.memoriavirtual.servicos.soap;
 
-import javax.ejb.EJB;
+import java.util.ArrayList;
 
+import javax.ejb.EJB;
+import javax.jws.WebService;
+
+import br.usp.memoriavirtual.modelo.entidades.bempatrimonial.BemPatrimonial;
+import br.usp.memoriavirtual.modelo.fachadas.ModeloException;
 import br.usp.memoriavirtual.modelo.fachadas.remoto.RealizarBuscaSimplesRemote;
 
-
+@WebService(serviceName="buscar")
 public class RealizarBuscaSOAPService {
 
 	@EJB
 	private RealizarBuscaSimplesRemote realizarBuscaEJB;
-	
-	
+
+	public ArrayList<BemPatrimonial> buscar(String stringDeBusca) {
+		ArrayList<BemPatrimonial> bensPatrimoniais = null;
+		try {
+			bensPatrimoniais = this.realizarBuscaEJB.buscar(stringDeBusca);
+		} catch (ModeloException e) {
+			e.printStackTrace();
+		}
+		return bensPatrimoniais;
+	}
 
 }
