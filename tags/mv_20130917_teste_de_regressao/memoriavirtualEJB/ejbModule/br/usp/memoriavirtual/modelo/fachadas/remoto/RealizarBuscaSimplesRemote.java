a1a74405569bcd8eb636ffed5727db56b441cc8d
tbianchi <tbianchi@699894a6-35f8-415d-b816-a1f94c44af83>
2013-09-18 18:04:03 +0000

    Versão para teste de regressão.
    git-svn-id: file:///home/brauner/Desktop/DEV_DUMP_19.05.2015/svn_storage/memoria_virtual_src@1086 699894a6-35f8-415d-b816-a1f94c44af83

---
+++ tags/mv_20130917_teste_de_regressao/memoriavirtualEJB/ejbModule/br/usp/memoriavirtual/modelo/fachadas/remoto/RealizarBuscaSimplesRemote.java
@@ -0,0 +1,44 @@
+package br.usp.memoriavirtual.modelo.fachadas.remoto;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.ejb.Remote;
+
+import br.usp.memoriavirtual.modelo.entidades.Instituicao;
+import br.usp.memoriavirtual.modelo.entidades.Multimidia;
+import br.usp.memoriavirtual.modelo.entidades.Usuario;
+import br.usp.memoriavirtual.modelo.entidades.bempatrimonial.BemArqueologico;
+import br.usp.memoriavirtual.modelo.entidades.bempatrimonial.BemArquitetonico;
+import br.usp.memoriavirtual.modelo.entidades.bempatrimonial.BemNatural;
+import br.usp.memoriavirtual.modelo.entidades.bempatrimonial.BemPatrimonial;
+import br.usp.memoriavirtual.modelo.fachadas.ModeloException;
+
+@Remote
+public interface RealizarBuscaSimplesRemote {
+
+	public ArrayList<BemPatrimonial> buscar(String busca)
+			throws ModeloException;
+
+	public BemArqueologico buscarBemArqueologico(BemPatrimonial bem)
+			throws ModeloException;
+
+	public BemArquitetonico buscarBemArquitetonico(BemPatrimonial bem)
+			throws ModeloException;
+
+	public BemNatural buscarBemNatural(BemPatrimonial bem)
+			throws ModeloException;
+
+	public List<Multimidia> buscarVideos(BemPatrimonial bem)
+			throws ModeloException;
+
+	public List<Multimidia> buscarImagens(BemPatrimonial bem)
+			throws ModeloException;
+
+	public List<Multimidia> buscarAudio(BemPatrimonial bem)
+			throws ModeloException;
+
+	public boolean possuiAcesso(Usuario usuario, Instituicao instituicao)
+			throws ModeloException;
+
+}
