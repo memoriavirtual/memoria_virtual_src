ceeec400a29ed4cafba5d1046f890aa904dd94c4
tbianchi <tbianchi@699894a6-35f8-415d-b816-a1f94c44af83>
2013-09-18 18:04:04 +0000

    Versão para teste de regressão.
    git-svn-id: file:///home/brauner/Desktop/DEV_DUMP_19.05.2015/svn_storage/memoria_virtual_src@1087 699894a6-35f8-415d-b816-a1f94c44af83

---
+++ tags/mv_20130917_teste_de_regressao/memoriavirtualWebService/src/br/usp/memoriavirtual/servicos/soap/RealizarBuscaSOAPService.java
@@ -0,0 +1,15 @@
+package br.usp.memoriavirtual.servicos.soap;
+
+import javax.ejb.EJB;
+
+import br.usp.memoriavirtual.modelo.fachadas.remoto.RealizarBuscaSimplesRemote;
+
+
+public class RealizarBuscaSOAPService {
+
+	@EJB
+	private RealizarBuscaSimplesRemote realizarBuscaEJB;
+	
+	
+
+}
