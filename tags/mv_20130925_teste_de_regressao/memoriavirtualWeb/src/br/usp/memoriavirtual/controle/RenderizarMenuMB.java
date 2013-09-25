5cd5892f5a300ab177d338c9db3ad146afae6bb0
joesantos <joesantos@699894a6-35f8-415d-b816-a1f94c44af83>
2013-09-25 17:07:44 +0000

    novo build
    git-svn-id: file:///home/brauner/Desktop/DEV_DUMP_19.05.2015/svn_storage/memoria_virtual_src@1101 699894a6-35f8-415d-b816-a1f94c44af83

---
+++ tags/mv_20130925_teste_de_regressao/memoriavirtualWeb/src/br/usp/memoriavirtual/controle/RenderizarMenuMB.java
@@ -0,0 +1,66 @@
+package br.usp.memoriavirtual.controle;
+
+import java.io.Serializable;
+import java.util.List;
+
+import javax.faces.context.FacesContext;
+import javax.faces.event.ComponentSystemEvent;
+import javax.servlet.http.HttpServletRequest;
+
+import br.usp.memoriavirtual.modelo.entidades.Acesso;
+import br.usp.memoriavirtual.modelo.entidades.Usuario;
+
+public class RenderizarMenuMB implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -3767548433370852588L;
+	public boolean administrador = false;
+	public boolean gerente = false;
+
+	@SuppressWarnings("unchecked")
+	public void verificarAcessos(ComponentSystemEvent event) {
+
+		/*
+		 * Pega a lista de acessos do usuario na sessao.
+		 */
+		HttpServletRequest request = (HttpServletRequest) FacesContext
+				.getCurrentInstance().getExternalContext().getRequest();
+
+		List<Acesso> listaAcessos = (List<Acesso>) request.getSession()
+				.getAttribute("acessos");
+
+		Usuario usuario = (Usuario) request.getSession()
+				.getAttribute("usuario");
+
+		if (usuario.isAdministrador() && usuario.isAtivo()) {
+			this.administrador = this.gerente = true;
+		} else {
+
+			for (Acesso acesso : listaAcessos) {
+				if (acesso.getGrupo().getId().equalsIgnoreCase("GERENTE")
+						&& acesso.getValidade()) {
+					this.gerente = true;
+				}
+			}
+		}
+	}
+
+	public boolean isAdministrador() {
+		return administrador;
+	}
+
+	public void setAdministrador(boolean administrador) {
+		this.administrador = administrador;
+	}
+
+	public boolean isGerente() {
+		return gerente;
+	}
+
+	public void setGerente(boolean gerente) {
+		this.gerente = gerente;
+	}
+
+}
