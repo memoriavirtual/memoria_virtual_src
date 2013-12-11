cc615962ef2823e87f24b3bc64ecb8e6983f6ee6
tbianchi <tbianchi@699894a6-35f8-415d-b816-a1f94c44af83>
2013-12-11 18:10:33 +0000

    Build mv_20131211_teste_de_regressao.
    git-svn-id: file:///home/brauner/Desktop/DEV_DUMP_19.05.2015/svn_storage/memoria_virtual_src@1144 699894a6-35f8-415d-b816-a1f94c44af83

--- trunk/memoriavirtualWebService/src/br/usp/memoriavirtual/servicos/MemoriaVirtualConfiguradorREST.java
+++ trunk/memoriavirtualWebService/src/br/usp/memoriavirtual/servicos/MemoriaVirtualConfiguradorREST.java
@@ -3,10 +3,12 @@ package br.usp.memoriavirtual.servicos;
 import java.util.HashSet;
 import java.util.Set;
 
+import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
 import br.usp.memoriavirtual.servicos.rest.RealizarBuscaRESTService;
 
+@ApplicationPath("rest/*")
 public class MemoriaVirtualConfiguradorREST extends Application {
 	public Set<Class<?>> getClasses() {
 		Set<Class<?>> classes = new HashSet<Class<?>>();
