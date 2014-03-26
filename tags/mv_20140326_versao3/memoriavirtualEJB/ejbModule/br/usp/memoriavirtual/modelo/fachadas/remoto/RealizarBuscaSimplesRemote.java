be265bd68ce67a31ef845b64551ec1259e301eac
psergiopoli <psergiopoli@699894a6-35f8-415d-b816-a1f94c44af83>
2014-03-26 18:40:20 +0000

    mv_20140326_versao3
    git-svn-id: file:///home/brauner/Desktop/DEV_DUMP_19.05.2015/svn_storage/memoria_virtual_src@1214 699894a6-35f8-415d-b816-a1f94c44af83

---
+++ tags/mv_20140326_versao3/memoriavirtualEJB/ejbModule/br/usp/memoriavirtual/modelo/fachadas/remoto/RealizarBuscaSimplesRemote.java
@@ -0,0 +1,32 @@
+package br.usp.memoriavirtual.modelo.fachadas.remoto;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.ejb.Remote;
+
+import br.usp.memoriavirtual.modelo.entidades.Instituicao;
+import br.usp.memoriavirtual.modelo.entidades.Multimidia;
+import br.usp.memoriavirtual.modelo.entidades.Usuario;
+import br.usp.memoriavirtual.modelo.entidades.bempatrimonial.BemPatrimonial;
+import br.usp.memoriavirtual.modelo.fachadas.ModeloException;
+
+@Remote
+public interface RealizarBuscaSimplesRemote {
+
+	public ArrayList<BemPatrimonial> buscar(String busca)
+			throws ModeloException;
+
+	public List<Multimidia> buscarVideos(BemPatrimonial bem)
+			throws ModeloException;
+
+	public List<Multimidia> buscarImagens(BemPatrimonial bem)
+			throws ModeloException;
+
+	public List<Multimidia> buscarAudio(BemPatrimonial bem)
+			throws ModeloException;
+
+	public boolean possuiAcesso(Usuario usuario, Instituicao instituicao)
+			throws ModeloException;
+
+}
