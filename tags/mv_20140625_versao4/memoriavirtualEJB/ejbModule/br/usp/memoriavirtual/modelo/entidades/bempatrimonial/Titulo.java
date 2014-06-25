43f88c6203b885d5e9894cb7d9ac4661bde06d02
psergiopoli <psergiopoli@699894a6-35f8-415d-b816-a1f94c44af83>
2014-06-25 17:26:00 +0000

    Build Tag: mv_20140625_versao4
    git-svn-id: file:///home/brauner/Desktop/DEV_DUMP_19.05.2015/svn_storage/memoria_virtual_src@1278 699894a6-35f8-415d-b816-a1f94c44af83

---
+++ tags/mv_20140625_versao4/memoriavirtualEJB/ejbModule/br/usp/memoriavirtual/modelo/entidades/bempatrimonial/Titulo.java
@@ -0,0 +1,95 @@
+package br.usp.memoriavirtual.modelo.entidades.bempatrimonial;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Transient;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlTransient;
+
+@Entity(name = "BEMPATRIMONIAL_TITULOS")
+@SequenceGenerator(name = "TITULO_ID", sequenceName = "TITULO_SEQ", allocationSize = 1)
+@XmlRootElement
+@XmlAccessorType(value = XmlAccessType.FIELD)
+public class Titulo implements Serializable {
+
+	public enum TipoTitulo {
+		equivalente, atribuido, subtitulo, anterior, posterior, suplemento, correlato
+	};
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "TITULO_ID")
+	private long id;
+
+	@XmlTransient
+	@ManyToOne(fetch = FetchType.EAGER)
+	private BemPatrimonial bemPatrimonial;
+	private TipoTitulo tipo = Titulo.TipoTitulo.anterior;
+	private String valor = "";
+	@Transient
+	private boolean select = false;
+
+	public Titulo() {
+
+	}
+
+	@XmlTransient
+	public BemPatrimonial getBempatrimonial() {
+		return bemPatrimonial;
+	}
+
+	public void setBempatrimonial(BemPatrimonial bempatrimonial) {
+		this.bemPatrimonial = bempatrimonial;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public BemPatrimonial getBemPatrimonial() {
+		return bemPatrimonial;
+	}
+
+	public void setBemPatrimonial(BemPatrimonial bemPatrimonial) {
+		this.bemPatrimonial = bemPatrimonial;
+	}
+
+	public String getValor() {
+		return valor;
+	}
+
+	public void setValor(String valor) {
+		this.valor = valor;
+	}
+
+	public boolean isSelect() {
+		return select;
+	}
+
+	public void setSelect(boolean select) {
+		this.select = select;
+	}
+
+	public TipoTitulo getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(TipoTitulo tipo) {
+		this.tipo = tipo;
+	}
+
+}
