8992587460082d761f2c37f4ba05dc195e48c440
psergiopoli <psergiopoli@699894a6-35f8-415d-b816-a1f94c44af83>
2014-07-18 15:28:29 +0000

    Defeito #535: Defeito consertado.
    git-svn-id: file:///home/brauner/Desktop/DEV_DUMP_19.05.2015/svn_storage/memoria_virtual_src@1287 699894a6-35f8-415d-b816-a1f94c44af83

--- trunk/memoriavirtualEJB/ejbModule/br/usp/memoriavirtual/modelo/fachadas/ExcluirBemPatrimonial.java
+++ trunk/memoriavirtualEJB/ejbModule/br/usp/memoriavirtual/modelo/fachadas/ExcluirBemPatrimonial.java
@@ -1,11 +1,15 @@
 package br.usp.memoriavirtual.modelo.fachadas;
 
+import javax.ejb.EJB;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
+import br.usp.memoriavirtual.modelo.entidades.Multimidia;
 import br.usp.memoriavirtual.modelo.entidades.bempatrimonial.BemPatrimonial;
 import br.usp.memoriavirtual.modelo.fachadas.remoto.ExcluirBemPatrimonialRemote;
+import br.usp.memoriavirtual.modelo.fachadas.remoto.UtilMultimidiaRemote;
+import br.usp.memoriavirtual.utils.MVModeloCamposMultimidia;
 
 @Stateless(mappedName = "ExcluirBemPatrimonial")
 public class ExcluirBemPatrimonial implements ExcluirBemPatrimonialRemote {
@@ -13,12 +17,23 @@ public class ExcluirBemPatrimonial implements ExcluirBemPatrimonialRemote {
 	@PersistenceContext(unitName = "memoriavirtual")
 	private EntityManager entityManager;
 
+	@EJB
+	private UtilMultimidiaRemote utilMultimidiaEJB;
+	
 	@Override
 	public void excluir(long id) throws ModeloException {
 
 		try {
-			BemPatrimonial bemPatrimonial = this.entityManager.find(
-					BemPatrimonial.class, id);
+			
+			BemPatrimonial bemPatrimonial = this.entityManager.find(BemPatrimonial.class, id);
+			
+			for(MVModeloCamposMultimidia midiaGenerica : utilMultimidiaEJB.listarCampos(bemPatrimonial.getContainerMultimidia())){
+				Multimidia midia = this.entityManager.find(Multimidia.class, midiaGenerica.getId());
+				this.entityManager.remove(midia);
+			}
+			
+			this.entityManager.remove(bemPatrimonial.getContainerMultimidia());
+			
 			this.entityManager.remove(bemPatrimonial);
 		} catch (Exception e) {
 			e.printStackTrace();
