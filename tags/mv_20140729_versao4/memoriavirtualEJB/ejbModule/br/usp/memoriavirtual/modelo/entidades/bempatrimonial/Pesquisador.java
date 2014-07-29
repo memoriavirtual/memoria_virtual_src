b68e944c820fd3e3b66b38a10bf205a97b829af4
tbianchi <tbianchi@699894a6-35f8-415d-b816-a1f94c44af83>
2014-07-29 17:16:39 +0000

    Build versão 4: mv_20140729_versao4
    git-svn-id: file:///home/brauner/Desktop/DEV_DUMP_19.05.2015/svn_storage/memoria_virtual_src@1294 699894a6-35f8-415d-b816-a1f94c44af83

---
+++ tags/mv_20140729_versao4/memoriavirtualEJB/ejbModule/br/usp/memoriavirtual/modelo/entidades/bempatrimonial/Pesquisador.java
@@ -0,0 +1,41 @@
+package br.usp.memoriavirtual.modelo.entidades.bempatrimonial;
+
+import java.io.Serializable;
+
+import javax.persistence.Embeddable;
+ 
+@Embeddable
+public class Pesquisador implements Serializable {
+
+	private static final long serialVersionUID = -5418736654663671679L;
+	private String nome = "";
+	private String dataPesquisa = "";
+	private String notasPesquisador = "";
+
+	public Pesquisador() {
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getDataPesquisa() {
+		return dataPesquisa;
+	}
+
+	public void setDataPesquisa(String dataPesquisa) {
+		this.dataPesquisa = dataPesquisa;
+	}
+
+	public String getNotasPesquisador() {
+		return notasPesquisador;
+	}
+
+	public void setNotasPesquisador(String notasPesquisador) {
+		this.notasPesquisador = notasPesquisador;
+	}
+}
