180261d1bdef55bb7a43e8699c1ec4d841dc9676
lucas <lucas@699894a6-35f8-415d-b816-a1f94c44af83>
2014-08-13 18:27:45 +0000

    tarefa #453
    git-svn-id: file:///home/brauner/Desktop/DEV_DUMP_19.05.2015/svn_storage/memoria_virtual_src@1306 699894a6-35f8-415d-b816-a1f94c44af83

--- trunk/memoriavirtualWeb/src/br/usp/memoriavirtual/controle/ExcluirBemPatrimonialMB.java
+++ trunk/memoriavirtualWeb/src/br/usp/memoriavirtual/controle/ExcluirBemPatrimonialMB.java
@@ -5,23 +5,25 @@ import java.io.Serializable;
 import javax.ejb.EJB;
 import javax.el.ELResolver;
 import javax.faces.bean.ManagedBean;
-import javax.faces.bean.ViewScoped;
+import javax.faces.bean.SessionScoped;
 import javax.faces.context.FacesContext;
 
 import br.usp.memoriavirtual.modelo.fachadas.remoto.ExcluirBemPatrimonialRemote;
 
 @ManagedBean(name = "excluirBemPatrimonialMB")
-@ViewScoped
+@SessionScoped
 public class ExcluirBemPatrimonialMB extends EditarBemPatrimonialMB implements
 		Serializable {
 
 	private static final long serialVersionUID = -5120759550692482010L;
 	private String id = "";
-	
+
 	@EJB
 	private ExcluirBemPatrimonialRemote excluirBemPatrimonialEJB;
 
 	private MensagensMB mensagens;
+	private String validade = "1";
+	private String justificativa = "";
 
 	public ExcluirBemPatrimonialMB() {
 		FacesContext facesContext = FacesContext.getCurrentInstance();
@@ -31,11 +33,13 @@ public class ExcluirBemPatrimonialMB extends EditarBemPatrimonialMB implements
 	}
 
 	public String selecionar() {
+		return this.redirecionar("/restrito/excluirbempatrimonial.jsf", true);
 
-		System.out.println("FOI");
-		return this
-				.redirecionar("/restrito/excluirbempatrimonial.jsf", true);
-		
+	}
+
+	public String solicitarExclusao() {
+
+		return null;
 	}
 
 	public String getId() {
@@ -53,4 +57,22 @@ public class ExcluirBemPatrimonialMB extends EditarBemPatrimonialMB implements
 	public void setMensagens(MensagensMB mensagens) {
 		this.mensagens = mensagens;
 	}
+
+	public String getValidade() {
+		return validade;
+	}
+
+	public void setValidade(String validade) {
+		this.validade = validade;
+	}
+
+	public String getJustificativa() {
+		return justificativa;
+	}
+
+	public void setJustificativa(String justificativa) {
+		this.justificativa = justificativa;
+	}
+	
+	
 }
