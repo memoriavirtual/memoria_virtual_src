597008e3348b2c1b5f6091b9945da069a7160536
tbianchi <tbianchi@699894a6-35f8-415d-b816-a1f94c44af83>
2014-08-13 19:11:17 +0000

    Atividade #453
    git-svn-id: file:///home/brauner/Desktop/DEV_DUMP_19.05.2015/svn_storage/memoria_virtual_src@1307 699894a6-35f8-415d-b816-a1f94c44af83

--- trunk/memoriavirtualWeb/src/br/usp/memoriavirtual/controle/ExcluirBemPatrimonialMB.java
+++ trunk/memoriavirtualWeb/src/br/usp/memoriavirtual/controle/ExcluirBemPatrimonialMB.java
@@ -1,19 +1,21 @@
 package br.usp.memoriavirtual.controle;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.ejb.EJB;
 import javax.el.ELResolver;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 import javax.faces.context.FacesContext;
+import javax.faces.model.SelectItem;
 
 import br.usp.memoriavirtual.modelo.fachadas.remoto.ExcluirBemPatrimonialRemote;
 
 @ManagedBean(name = "excluirBemPatrimonialMB")
 @SessionScoped
-public class ExcluirBemPatrimonialMB extends EditarBemPatrimonialMB implements
-		Serializable {
+public class ExcluirBemPatrimonialMB extends EditarBemPatrimonialMB implements Serializable {
 
 	private static final long serialVersionUID = -5120759550692482010L;
 	private String id = "";
@@ -24,12 +26,12 @@ public class ExcluirBemPatrimonialMB extends EditarBemPatrimonialMB implements
 	private MensagensMB mensagens;
 	private String validade = "1";
 	private String justificativa = "";
+	private String usuarioAprovador = "";
 
 	public ExcluirBemPatrimonialMB() {
 		FacesContext facesContext = FacesContext.getCurrentInstance();
 		ELResolver resolver = facesContext.getApplication().getELResolver();
-		this.mensagens = (MensagensMB) resolver.getValue(
-				facesContext.getELContext(), null, "mensagensMB");
+		this.mensagens = (MensagensMB) resolver.getValue(facesContext.getELContext(), null, "mensagensMB");
 	}
 
 	public String selecionar() {
@@ -66,13 +68,44 @@ public class ExcluirBemPatrimonialMB extends EditarBemPatrimonialMB implements
 		this.validade = validade;
 	}
 
+	/**
+	 * @return the justificativa
+	 */
 	public String getJustificativa() {
 		return justificativa;
 	}
 
+	/**
+	 * @param justificativa
+	 *           the justificativa to set
+	 */
 	public void setJustificativa(String justificativa) {
 		this.justificativa = justificativa;
 	}
-	
-	
+
+	/**
+	 * @return the usuarioAprovador
+	 */
+	public String getUsuarioAprovador() {
+		return usuarioAprovador;
+	}
+
+	/**
+	 * @param usuarioAprovador
+	 *           the usuarioAprovador to set
+	 */
+	public void setUsuarioAprovador(String usuarioAprovador) {
+		this.usuarioAprovador = usuarioAprovador;
+	}
+
+	/**
+	 * Retorna a lista de usuários que serão mostrados no campo "Select"
+	 * 
+	 * @return
+	 */
+	public List<SelectItem> getUsuariosAprovadores() {
+		List<SelectItem> usuarios = new ArrayList<SelectItem>();
+
+		return usuarios;
+	}
 }
