8ae3d5fdbbc65a3b8a2b7ecb662b52f2a14ecaef
daniboscolo <daniboscolo@699894a6-35f8-415d-b816-a1f94c44af83>
2014-09-17 20:05:08 +0000

    Adicionado tela de Login.
    git-svn-id: file:///home/brauner/Desktop/DEV_DUMP_19.05.2015/svn_storage/memoria_virtual_src@1328 699894a6-35f8-415d-b816-a1f94c44af83

--- trunk/memoriavirtualMobile/MVmobile/Mvmobile/app/build/generated/source/r/debug/br/usp/mvmobile/R.java
+++ trunk/memoriavirtualMobile/MVmobile/Mvmobile/app/build/generated/source/r/debug/br/usp/mvmobile/R.java
@@ -18,22 +18,31 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int action_settings=0x7f050001;
+        public static final int content_fragment=0x7f050000;
     }
     public static final class layout {
-        public static final int activity_main=0x7f030000;
+        public static final int activity_login=0x7f030000;
+        public static final int activity_main=0x7f030001;
+        public static final int fragment_form=0x7f030002;
     }
     public static final class menu {
-        public static final int main=0x7f070000;
+        public static final int login=0x7f080000;
+        public static final int main=0x7f080001;
     }
     public static final class string {
-        public static final int action_settings=0x7f050000;
-        public static final int app_name=0x7f050001;
-        public static final int hello_world=0x7f050002;
+        public static final int action_settings=0x7f060000;
+        public static final int app_name=0x7f060001;
+        public static final int hello_blank_fragment=0x7f060002;
+        public static final int hello_world=0x7f060003;
+        public static final int login_organization=0x7f060004;
+        public static final int login_password=0x7f060005;
+        public static final int login_user=0x7f060006;
+        public static final int title_activity_login=0x7f060007;
     }
     public static final class style {
         /**  Customize your theme here. 
          */
-        public static final int AppTheme=0x7f060000;
+        public static final int AppTheme=0x7f070000;
     }
 }
