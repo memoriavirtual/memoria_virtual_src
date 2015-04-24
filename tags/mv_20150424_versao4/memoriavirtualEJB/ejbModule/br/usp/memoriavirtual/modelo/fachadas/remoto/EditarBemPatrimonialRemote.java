9f1db822aaa4932c8fdb8d74e7dbcf172e7230c0
psergiopoli <psergiopoli@699894a6-35f8-415d-b816-a1f94c44af83>
2015-04-24 18:52:20 +0000

    Erro ao criar nova Tag
    git-svn-id: file:///home/brauner/Desktop/DEV_DUMP_19.05.2015/svn_storage/memoria_virtual_src@1438 699894a6-35f8-415d-b816-a1f94c44af83

--- tags/mv_20150424_versao4/memoriavirtualEJB/ejbModule/br/usp/memoriavirtual/modelo/fachadas/remoto/EditarBemPatrimonialRemote.java
+++
@@ -1,8 +0,0 @@
-package br.usp.memoriavirtual.modelo.fachadas.remoto;
-
-import br.usp.memoriavirtual.modelo.entidades.bempatrimonial.BemPatrimonial;
-import br.usp.memoriavirtual.modelo.fachadas.ModeloException;
-
-public interface EditarBemPatrimonialRemote {
-	public void editarBemPatrimonial(BemPatrimonial bem) throws ModeloException;
-}
--- tags/mv_20150424_versao4/memoriavirtualWeb/src/br/usp/memoriavirtual/utils/MVControleMemoriaVirtual.java
+++
@@ -1,7 +0,0 @@
-package br.usp.memoriavirtual.utils;
-
-public class MVControleMemoriaVirtual {
-
-	public static String bundleName = "mensagens";
-	
-}
