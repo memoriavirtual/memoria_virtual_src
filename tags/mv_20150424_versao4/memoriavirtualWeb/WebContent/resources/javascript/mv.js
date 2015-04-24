b640a47838de3c4ede06cdb4f7024c0d26bbd26c
psergiopoli <psergiopoli@699894a6-35f8-415d-b816-a1f94c44af83>
2015-04-24 19:12:56 +0000

    Nova tag mv_20150424_versao4
    git-svn-id: file:///home/brauner/Desktop/DEV_DUMP_19.05.2015/svn_storage/memoria_virtual_src@1451 699894a6-35f8-415d-b816-a1f94c44af83

---
+++ tags/mv_20150424_versao4/memoriavirtualWeb/WebContent/resources/javascript/mv.js
@@ -0,0 +1,86 @@
+function mensagensSucesso(mensagem) {
+	$('#mensagens').attr('class', 'text-success alert alert-success');
+	$('#mensagens').find('span').text(mensagem);
+	$('#mensagens').show('slow');
+}
+
+function limparMensagens() {
+	var mensagens = $('#mensagens');
+	mensagens.slideUp('slow');
+	$.get('/memoriavirtual/limparmensagens', function() {
+	});
+}
+
+function selecionarAbaInstituicao() {
+	selecionarAba("2");
+}
+
+function selecionarAbaPatrimonio() {
+	selecionarAba("3");
+}
+
+function selecionarAbaRevisao() {
+	selecionarAba("4");
+}
+
+function selecionarAbaAutor() {
+	selecionarAba("5");
+}
+
+function selecionarAbaAdministracao() {
+	selecionarAba("6");
+}
+
+function selecionarAbaConta() {
+	selecionarAba("7");
+}
+
+function selecionarAba(i) {
+	todosItens = $('.linkPrincipal');
+	todosItens.removeClass('itemAtual');
+	todosItens.addClass('outrosItens');
+
+	segundoItemAnchor = $("#menuNavegacao li:nth-of-type(" + i + ")").find(
+			'.linkPrincipal');
+	segundoItemAnchor.addClass('itemAtual');
+
+	filhos = $('#menuNavegacao li:nth-of-type(' + i + ')').find(
+			'.linkSecundario').clone();
+	subMenu = $('#subMenu');
+	subMenu.html(filhos);
+}
+
+function mostrarErro(messagesId) {
+
+	var messages = $(messagesId);
+
+	var texto = messages.text();
+	var formGroup = messages.closest('.form-group');
+
+	if (texto !== '') {
+		if (messages.hasClass('has-error')) {
+			formGroup.addClass('has-error');
+			formGroup.removeClass('has-warning');
+			formGroup.removeClass('has-success');
+		} else if (messages.hasClass('has-warning')) {
+			formGroup.addClass('has-warning');
+			formGroup.removeClass('has-error');
+			formGroup.removeClass('has-success');
+		} else if (messages.hasClass('has-success')) {
+			formGroup.addClass('has-success');
+			formGroup.removeClass('has-error');
+			formGroup.removeClass('has-warning');
+		}
+	} else {
+		formGroup.removeClass('has-error');
+		formGroup.removeClass('has-warning');
+		formGroup.removeClass('has-success');
+	}
+}
+
+$('document').ready(function() {
+	$('.container-expansivo-label').click(function(e){
+		e.preventDefault();
+		$(this).siblings('.container-expansivo-conteudo').toggle('slow');
+	});
+});
---
+++ tags/mv_20150424_versao4/memoriavirtualWeb/src/br/usp/memoriavirtual/utils/MVModeloEmailTemplates.java
@@ -0,0 +1,19 @@
+package br.usp.memoriavirtual.utils;
+
+public enum MVModeloEmailTemplates {
+	excluirInstituicao("excluirInstituicao.html"), 
+	enviarConvite("enviarConvite.html"), 
+	editarCadastroUsuario("solicitarEdicaoUsuario.html"), 
+	excluirUsuario("solicitarExclusaoUsuario.html"),
+	excluirBem("excluirBemPatrimonial.html");
+
+	private String local;
+
+	private MVModeloEmailTemplates(String local) {
+		this.local = local;
+	}
+
+	public String toString() {
+		return local;
+	}
+}
