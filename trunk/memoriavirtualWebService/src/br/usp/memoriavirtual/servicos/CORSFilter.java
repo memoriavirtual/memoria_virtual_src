628ee2694d03b97dca4190866d13bc08932cdbef
tbianchi <tbianchi@tbianchi-ibm-laptop>
2017-05-10 15:53:59 -0300

    Configuração CORS Filter pelo console do servidor de aplicações utilizando uma variável JNDI.

--- trunk/memoriavirtualWebService/src/br/usp/memoriavirtual/servicos/CORSFilter.java
+++ trunk/memoriavirtualWebService/src/br/usp/memoriavirtual/servicos/CORSFilter.java
@@ -1,13 +1,19 @@
 package br.usp.memoriavirtual.servicos;
 
 import java.io.IOException;
+import java.util.Properties;
 
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
 import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.container.ContainerResponseContext;
 import javax.ws.rs.container.ContainerResponseFilter;
 import javax.ws.rs.container.PreMatching;
 import javax.ws.rs.core.MultivaluedMap;
 import javax.ws.rs.ext.Provider;
+
+import br.usp.memoriavirtual.modelo.fachadas.ModeloException;
 
 @Provider
 @PreMatching
@@ -18,7 +24,23 @@ public class CORSFilter implements ContainerResponseFilter {
 			throws IOException {
 		final MultivaluedMap<String, Object> headers = responseContext.getHeaders();
 
-		headers.add("Access-Control-Allow-Origin", "*");
+		Context context = null;
+		try {
+			context = new InitialContext();
+		} catch (NamingException e) {
+			new ModeloException(e);
+		}
+		Properties propriedades = null;
+		try {
+			propriedades = (Properties) context.lookup("memoriavirtual.propriedades");
+		} catch (NamingException e) {
+			new ModeloException(e);
+		}
+
+		String dominiosPermitidos = propriedades.getProperty("dominiosPermitidos") != null
+				? propriedades.getProperty("dominiosPermitidos") : "";
+
+		headers.add("Access-Control-Allow-Origin", dominiosPermitidos);
 		headers.add("Access-Control-Allow-Headers", "Authorization, Origin, X-Requested-With, Content-Type");
 		headers.add("Access-Control-Expose-Headers", "Location, Content-Disposition");
 		headers.add("Access-Control-Allow-Methods", "POST, PUT, GET, DELETE, HEAD, OPTIONS");
